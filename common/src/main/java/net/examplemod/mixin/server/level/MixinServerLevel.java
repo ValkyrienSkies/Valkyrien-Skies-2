266f7d8d680da631b4533c1fde006975ea2038bf
Tri0de <best108@uw.edu>
2021-01-31 23:20:50 -0800

    Made QueryableShipData and ChunkAllocator load/save with each Level

--- common/src/main/java/net/examplemod/mixin/server/level/MixinServerLevel.java
+++ common/src/main/java/net/examplemod/mixin/server/level/MixinServerLevel.java
@@ -1,27 +1,32 @@
 package net.examplemod.mixin.server.level;
 
 import net.examplemod.IShipObjectWorldProvider;
+import net.examplemod.ShipSavedData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.storage.DimensionDataStorage;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.valkyrienskies.core.game.ChunkAllocator;
-import org.valkyrienskies.core.game.QueryableShipData;
 import org.valkyrienskies.core.game.ShipObjectWorld;
 
 @Mixin(ServerLevel.class)
-public class MixinServerLevel implements IShipObjectWorldProvider {
+public abstract class MixinServerLevel implements IShipObjectWorldProvider {
 
-    private final ShipObjectWorld shipObjectWorld = new ShipObjectWorld(new QueryableShipData(), new ChunkAllocator(-7000, 3000));
+    private ShipObjectWorld shipObjectWorld = null;
+    private ShipSavedData shipSavedData = null;
 
     @Inject(
             at = @At("TAIL"),
             method = "<init>"
     )
-    private void inject(CallbackInfo info) {
-        // TODO: Load QueryableShipData from disk here
+    private void postConstructor(CallbackInfo info) {
+        // Load ship data from the world storage
+        shipSavedData = getDataStorage().computeIfAbsent(ShipSavedData.Companion::createNewEmptyShipSavedData, ShipSavedData.SAVED_DATA_ID);
+        // Make a ship world using the loaded ship data
+        shipObjectWorld = new ShipObjectWorld(shipSavedData.getQueryableShipData(), shipSavedData.getChunkAllocator());
     }
 
     @NotNull
@@ -29,4 +34,7 @@ public class MixinServerLevel implements IShipObjectWorldProvider {
     public ShipObjectWorld getShipObjectWorld() {
         return shipObjectWorld;
     }
+
+    @Shadow
+    public abstract DimensionDataStorage getDataStorage();
 }
