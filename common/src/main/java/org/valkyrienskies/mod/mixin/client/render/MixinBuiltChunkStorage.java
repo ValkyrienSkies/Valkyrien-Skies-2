b1e71627eb77968086516477bac839b3f7089c83
Tri0de <best108@uw.edu>
2021-02-23 05:04:31 -0800

    Made BuiltChunkStorage store ship chunks

---
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/render/MixinBuiltChunkStorage.java
@@ -0,0 +1,78 @@
+package org.valkyrienskies.mod.mixin.client.render;
+
+import io.netty.util.collection.LongObjectHashMap;
+import io.netty.util.collection.LongObjectMap;
+import net.minecraft.client.render.BuiltChunkStorage;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.chunk.ChunkBuilder;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.valkyrienskies.mod.VSGameUtils;
+
+/**
+ * The purpose of this mixin is to allow {@link BuiltChunkStorage} to render ship chunks.
+ */
+@Mixin(BuiltChunkStorage.class)
+public class MixinBuiltChunkStorage {
+
+    @Shadow
+    @Final
+    protected World world;
+
+    @Shadow
+    protected int sizeY;
+
+    // Maps chunk position to an array of BuiltChunk, indexed by the y value.
+    private final LongObjectMap<ChunkBuilder.BuiltChunk[]> vs$shipRenderChunks = new LongObjectHashMap<>();
+    private ChunkBuilder vs$chunkBuilder;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void preInit(ChunkBuilder chunkBuilder, World world, int viewDistance, WorldRenderer worldRenderer, CallbackInfo callbackInfo) {
+        this.vs$chunkBuilder = chunkBuilder;
+    }
+
+    @Inject(method = "scheduleRebuild", at = @At("HEAD"), cancellable = true)
+    private void preScheduleRebuild(int x, int y, int z, boolean important, CallbackInfo callbackInfo) {
+        if (y < 0 || y >= sizeY) return; // Weird, but just ignore it
+        if (VSGameUtils.INSTANCE.getShipObjectWorldFromWorld(world).getChunkAllocator().isChunkInShipyard(x, z)) {
+            final long chunkPosAsLong = ChunkPos.toLong(x, z);
+            if (!vs$shipRenderChunks.containsKey(chunkPosAsLong))
+                vs$shipRenderChunks.put(chunkPosAsLong, new ChunkBuilder.BuiltChunk[sizeY]);
+            final ChunkBuilder.BuiltChunk[] renderChunksArray = vs$shipRenderChunks.get(chunkPosAsLong);
+
+            if (renderChunksArray[y] == null) {
+                renderChunksArray[y] = vs$chunkBuilder.new BuiltChunk();
+            }
+            callbackInfo.cancel();
+        }
+    }
+
+    @Inject(method = "getRenderedChunk", at = @At("HEAD"), cancellable = true)
+    private void preGetRenderedChunk(BlockPos pos, CallbackInfoReturnable<ChunkBuilder.BuiltChunk> callbackInfoReturnable) {
+        final int chunkX = MathHelper.floorDiv(pos.getX(), 16);
+        final int chunkY = MathHelper.floorDiv(pos.getY(), 16);
+        final int chunkZ = MathHelper.floorDiv(pos.getZ(), 16);
+
+        if (chunkY < 0 || chunkY >= sizeY) return; // Weird, but ignore it
+
+        if (VSGameUtils.INSTANCE.getShipObjectWorldFromWorld(world).getChunkAllocator().isChunkInShipyard(chunkX, chunkZ)) {
+            final long chunkPosAsLong = ChunkPos.toLong(chunkX, chunkZ);
+            final ChunkBuilder.BuiltChunk[] renderChunksArray = vs$shipRenderChunks.get(chunkPosAsLong);
+            if (renderChunksArray == null) {
+                callbackInfoReturnable.setReturnValue(null);
+                return;
+            }
+            final ChunkBuilder.BuiltChunk renderChunk = renderChunksArray[chunkY];
+            callbackInfoReturnable.setReturnValue(renderChunk);
+        }
+    }
+}
