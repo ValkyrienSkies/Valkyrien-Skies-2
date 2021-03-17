c860780b456c33c00630983b45c43d68c6a7ad20
Tri0de <best108@uw.edu>
2021-03-17 00:29:15 -0700

    Fixed fabric server crashing when launched

--- fabric/src/main/kotlin/org/valkyrienskies/mod/fabric/common/VSFabricNetworking.kt
+++ fabric/src/main/kotlin/org/valkyrienskies/mod/fabric/common/VSFabricNetworking.kt
@@ -11,16 +11,14 @@ import org.valkyrienskies.mod.common.ValkyrienSkiesMod
 object VSFabricNetworking {
     internal val VS_PACKET_ID = Identifier(ValkyrienSkiesMod.MOD_ID, "vs_packet")
 
-    internal fun registerFabricNetworking() {
-        registerClientPacketHandlers()
-        injectFabricPacketSenders()
-    }
-
-    private fun registerClientPacketHandlers() {
+    /**
+     * Only run on client
+     */
+    internal fun registerClientPacketHandlers() {
         ClientPlayNetworking.registerGlobalReceiver(VS_PACKET_ID, VSClientPlayChannelHandler)
     }
 
-    private fun injectFabricPacketSenders() {
+    internal fun injectFabricPacketSenders() {
         VSNetworking.shipDataPacketToClientSender = VSFabricServerToClientPacketSender
     }
 }
