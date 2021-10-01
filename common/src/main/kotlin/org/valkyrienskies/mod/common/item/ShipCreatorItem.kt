ec780c65b5043f3167f34715ef4b06386dae491b
StewStrong <stew.strong.420@gmail.com>
2021-10-01 06:15:33 -0600

    Fixed ship center block being too heavy

--- common/src/main/kotlin/org/valkyrienskies/mod/common/item/ShipCreatorItem.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/item/ShipCreatorItem.kt
@@ -7,7 +7,6 @@ import net.minecraft.item.ItemUsageContext
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.ActionResult
 import org.joml.Vector3i
-import org.valkyrienskies.mod.common.BlockStateInfoProvider
 import org.valkyrienskies.mod.common.shipObjectWorld
 import org.valkyrienskies.mod.common.util.toBlockPos
 import org.valkyrienskies.mod.common.util.toJOML
@@ -32,8 +31,6 @@ class ShipCreatorItem(properties: Settings, private val scale: Double) : Item(pr
                 // Move the block from the world to a ship
                 level.setBlockState(blockPos, Blocks.AIR.defaultState, 11)
                 level.setBlockState(centerPos, blockState, 11)
-
-                BlockStateInfoProvider.onSetBlock(level, centerPos, Blocks.AIR.defaultState, blockState)
             }
         }
 
