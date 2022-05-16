2920ddd6f7052519fa3a2c56cf30d639c3b86d6a
StewStrong <stew.strong.420@gmail.com>
2022-05-16 01:04:42 -0600

    Fixed HUD PhysTps overlay

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/gui/hud/MixinInGameHud.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/gui/hud/MixinInGameHud.java
@@ -11,7 +11,6 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,8 +18,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.core.game.VSOptions;
-import org.valkyrienskies.core.game.ships.ShipObjectServerWorld;
-import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.core.pipelines.VSPipeline;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
@@ -46,13 +44,9 @@ public class MixinInGameHud {
 
         final IntegratedServer integratedServer = this.client.getServer();
         if (integratedServer != null) {
-            for (final ServerWorld serverWorld : integratedServer.getWorlds()) {
-                final ShipObjectServerWorld shipObjectServerWorld = VSGameUtilsKt.getShipObjectWorld(serverWorld);
-                final String worldName = serverWorld.getRegistryKey().getValue().toString();
-                final double physicsTPS = 0.0; // TODO, add this back later
-                final String worldPhysicsDebugText = worldName + " PhysTPS: " + physicsTPS;
-                debugText.add(worldPhysicsDebugText);
-            }
+            final double physicsTPS = VSPipeline.Companion.getVSPipeline().computePhysTps();
+            final String worldPhysicsDebugText = "VS PhysTPS: " + physicsTPS;
+            debugText.add(worldPhysicsDebugText);
         }
 
         for (int i = 0; i < debugText.size(); i++) {
