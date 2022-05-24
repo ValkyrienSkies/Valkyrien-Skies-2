fc228b2ef0d4b99a1274f372a8198f60ea503f48
StewStrong <stew.strong.420@gmail.com>
2022-05-24 05:16:25 -0600

    Added ShipDataClient

--- common/src/main/kotlin/org/valkyrienskies/mod/common/networking/impl/VSPacketShipDataClientHandler.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/networking/impl/VSPacketShipDataClientHandler.kt
@@ -1,6 +1,7 @@
 package org.valkyrienskies.mod.common.networking.impl
 
 import net.minecraft.client.MinecraftClient
+import org.valkyrienskies.core.game.ships.ShipDataClient
 import org.valkyrienskies.core.networking.IVSPacket
 import org.valkyrienskies.core.networking.IVSPacketClientHandler
 import org.valkyrienskies.core.networking.impl.VSPacketShipDataList
@@ -15,8 +16,11 @@ object VSPacketShipDataClientHandler : IVSPacketClientHandler {
 
         vsPacket.shipDataList.forEach {
             if (shipWorld.queryableShipData.getShipDataFromUUID(it.shipUUID) == null) {
-                shipWorld.queryableShipData.addShipData(it)
+                // Convert [ShipDataCommon] to [ShipDataClient]
+                val shipDataClient = ShipDataClient.Companion.createShipDataClientFromShipDataCommon(it)
+                shipWorld.queryableShipData.addShipData(shipDataClient)
             } else {
+                // Update the ship transform
                 shipWorld.queryableShipData.getShipDataFromUUID(it.shipUUID)!!.shipTransform = it.shipTransform
             }
         }
