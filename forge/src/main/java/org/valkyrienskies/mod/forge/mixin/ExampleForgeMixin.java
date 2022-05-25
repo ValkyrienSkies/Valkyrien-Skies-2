e045b9cd58d5c4a4fe700edbdf410d3bfe97c6d7
StewStrong <stew.strong.420@gmail.com>
2022-05-25 01:06:44 -0600

    Updated fabric and architectury and forge, and disabled forge mixins

--- forge/src/main/java/org/valkyrienskies/mod/forge/mixin/ExampleForgeMixin.java
+++ forge/src/main/java/org/valkyrienskies/mod/forge/mixin/ExampleForgeMixin.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class ExampleForgeMixin {
@@ -13,7 +16,6 @@ public class ExampleForgeMixin {
     @Final
     private static Logger LOGGER;
 
-    /*
     @Inject(
         at = @At("HEAD"),
         method = "init"
@@ -21,6 +23,5 @@ public class ExampleForgeMixin {
     public void inject(final CallbackInfo info) {
         LOGGER.info("Hello from ExampleForgeMixin");
     }
-     */
 
 }
