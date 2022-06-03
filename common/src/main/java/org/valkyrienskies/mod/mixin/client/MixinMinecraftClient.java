1d2c4ff7c3a319a02fce2acab7abc4492ccb4921
ewoudje <ewoud.dierickx@hotmail.be>
2022-06-03 13:59:52 +0200

    Better mixin's and froge couldn't find one mixin so this fixes that too

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/MixinMinecraftClient.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/MixinMinecraftClient.java
@@ -1,29 +1,19 @@
 package org.valkyrienskies.mod.mixin.client;
 
-import com.mojang.datafixers.util.Function4;
-import java.util.function.Function;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.resource.DataPackSettings;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.registry.DynamicRegistryManager;
-import net.minecraft.world.SaveProperties;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.mod.common.PlayerUtil;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
-import org.valkyrienskies.mod.event.RegistryEvents;
 import org.valkyrienskies.mod.mixinducks.client.MinecraftClientDuck;
 
 @Mixin(MinecraftClient.class)
@@ -55,21 +45,5 @@ public class MixinMinecraftClient implements MinecraftClientDuck {
         return PlayerUtil.INSTANCE.transformPlayerTemporarily(player,
             VSGameUtilsKt.getShipObjectManagingPos(world, hitResult.getBlockPos()),
             () -> receiver.interactBlock(player, world, hand, (BlockHitResult) originalCrosshairTarget));
-    }
-
-    @Inject(
-        method = "startIntegratedServer(Ljava/lang/String;Lnet/minecraft/util/registry/DynamicRegistryManager$Impl;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/resource/ServerResourceManager;loadRegistryTags()V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void afterTags(final String string, final DynamicRegistryManager.Impl impl,
-        final Function<LevelStorage.Session, DataPackSettings> function,
-        final Function4<LevelStorage.Session, DynamicRegistryManager.Impl, ResourceManager, DataPackSettings, SaveProperties> function4,
-        final boolean bl, final MinecraftClient.WorldLoadAction worldLoadAction, final CallbackInfo ci) {
-
-        RegistryEvents.tagsAreLoaded();
     }
 }
