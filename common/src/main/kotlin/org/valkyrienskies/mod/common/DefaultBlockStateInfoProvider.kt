c1dc1061548b9677c55db8e007624d238b240f18
StewStrong <stew.strong.420@gmail.com>
2022-06-05 16:42:23 -0600

    Fixed physics treating grass and snow as solid blocks

--- common/src/main/kotlin/org/valkyrienskies/mod/common/DefaultBlockStateInfoProvider.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/DefaultBlockStateInfoProvider.kt
@@ -1,12 +1,12 @@
 package org.valkyrienskies.mod.common
 
 import net.minecraft.block.BlockState
-import net.minecraft.block.FluidBlock
+import net.minecraft.block.Material
 import org.valkyrienskies.core.game.VSBlockType
 import org.valkyrienskies.core.game.VSBlockType.AIR
+import org.valkyrienskies.core.game.VSBlockType.LAVA
 import org.valkyrienskies.core.game.VSBlockType.SOLID
 import org.valkyrienskies.core.game.VSBlockType.WATER
-import org.valkyrienskies.mod.mixin.accessors.block.AbstractBlockAccessor
 
 object DefaultBlockStateInfoProvider : BlockStateInfoProvider {
     override val priority: Int
@@ -18,19 +18,11 @@ object DefaultBlockStateInfoProvider : BlockStateInfoProvider {
     }
 
     override fun getBlockStateType(blockState: BlockState): VSBlockType {
-        return when {
-            blockState.isAir -> {
-                AIR
-            }
-            !((blockState.block as AbstractBlockAccessor).isCollidable) -> {
-                AIR
-            }
-            blockState.block is FluidBlock -> {
-                WATER
-            }
-            else -> {
-                SOLID
-            }
-        }
+        if (blockState.isAir) return AIR
+
+        val blockMaterial = blockState.material
+        if (blockMaterial.isLiquid)
+            return if (blockMaterial == Material.LAVA) LAVA else WATER
+        return if (blockMaterial.isSolid) SOLID else AIR
     }
 }
