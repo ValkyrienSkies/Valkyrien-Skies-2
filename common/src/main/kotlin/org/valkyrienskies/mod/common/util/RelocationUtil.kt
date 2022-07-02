4f9774da489f9f716756e3acc2d01d44dc291051
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-03 00:30:07 +0200

    Well be doing it the *safe* way

--- common/src/main/kotlin/org/valkyrienskies/mod/common/util/RelocationUtil.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/util/RelocationUtil.kt
@@ -1,21 +1,40 @@
 package org.valkyrienskies.mod.common.util
 
 import net.minecraft.core.BlockPos
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.Container
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.chunk.LevelChunk
+import net.minecraft.world.level.chunk.LevelChunk.EntityCreationType.CHECK
+
+private val AIR = Blocks.AIR.defaultBlockState()
 
 // the from and the to can be local or global
 fun relocateBlock(fromChunk: LevelChunk, from: BlockPos, toChunk: LevelChunk, to: BlockPos) {
     val state = fromChunk.getBlockState(from)
     val entity = fromChunk.getBlockEntity(from)
 
-    fromChunk.setBlockState(from, Blocks.AIR.defaultBlockState(), false)
+    val tag = entity?.let {
+        val tag = CompoundTag()
+        it.save(tag)
+        tag.putInt("x", to.x)
+        tag.putInt("y", to.y)
+        tag.putInt("z", to.z)
+
+        // so that it won't drop its contents
+        if (it is Container) {
+            it.clearContent()
+        }
+
+        tag
+    }
+
+    fromChunk.setBlockState(from, AIR, false)
     toChunk.setBlockState(to, state, false) // TODO should isMoving be false?
-    entity?.let {
-        it.setLevelAndPosition(toChunk.level, to)
-        fromChunk.blockEntities.remove(from)
-        toChunk.addBlockEntity(it)
+
+    tag?.let {
+        toChunk.getBlockEntity(to, CHECK)!!.load(state, tag)
     }
 }
 
