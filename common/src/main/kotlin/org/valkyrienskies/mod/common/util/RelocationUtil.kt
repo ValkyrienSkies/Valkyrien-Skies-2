99a23d2493aedf27a9527850ada663cf81df1ed2
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-02 14:49:55 +0200

    Testing

--- common/src/main/kotlin/org/valkyrienskies/mod/common/util/RelocationUtil.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/util/RelocationUtil.kt
@@ -2,6 +2,7 @@ package org.valkyrienskies.mod.common.util
 
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.chunk.LevelChunk
 
 // the from and the to can be local or global
@@ -9,6 +10,7 @@ fun relocateBlock(fromChunk: LevelChunk, from: BlockPos, toChunk: LevelChunk, to
     val state = fromChunk.getBlockState(from)
     val entity = fromChunk.getBlockEntity(from)
 
+    fromChunk.setBlockState(from, Blocks.AIR.defaultBlockState(), false)
     toChunk.setBlockState(to, state, false) // TODO should isMoving be false?
     entity?.let {
         it.setLevelAndPosition(toChunk.level, to)
