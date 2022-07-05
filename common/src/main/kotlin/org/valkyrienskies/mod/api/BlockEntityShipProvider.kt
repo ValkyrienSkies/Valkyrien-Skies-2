2108b1be3ce001f4ab31f7d4550220981e708b64
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-05 09:26:31 +0200

    Ship API (#59)
    * ShipableBlockEntity Interface
    
    * How about this!
    
    * New ideas, new interfaces
    
    * BlockEntityShipProvider
    
    * Small refactor fix
    
    * The good changes that ruby proposed
    
    * field ship of ShipProvider should be nullable
    
    * Updated vs-core
    
    Co-authored-by: StewStrong <stew.strong.420@gmail.com>

---
+++ common/src/main/kotlin/org/valkyrienskies/mod/api/BlockEntityShipProvider.kt
@@ -0,0 +1,22 @@
+package org.valkyrienskies.mod.api
+
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.entity.BlockEntity
+import org.valkyrienskies.core.api.Ship
+import org.valkyrienskies.core.api.ShipProvider
+import org.valkyrienskies.core.game.ships.ShipObject
+
+interface BlockEntityShipProvider : ShipProvider {
+    override var ship: Ship?
+
+    /**
+     * Gets called on block entity creation and relocation
+     *  the actual new [BlockPos], [Ship] and [Level] shall be assigned
+     *  on this [BlockEntity] after this call
+     * @param newPos
+     * @param newLevel
+     * @param newShip
+     */
+    fun onShipChange(newPos: BlockPos, newLevel: Level, newShip: ShipObject) {}
+}
