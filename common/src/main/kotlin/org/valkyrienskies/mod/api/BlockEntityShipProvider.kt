baa667567aefe061b01ad4c211d38ee315f175c7
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-10 16:18:01 +0200

    Implementing the full Ship API (#86)

--- common/src/main/kotlin/org/valkyrienskies/mod/api/BlockEntityShipProvider.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/api/BlockEntityShipProvider.kt
@@ -1,22 +1,11 @@
 package org.valkyrienskies.mod.api
 
-import net.minecraft.core.BlockPos
-import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.entity.BlockEntity
 import org.valkyrienskies.core.api.Ship
 import org.valkyrienskies.core.api.ShipProvider
-import org.valkyrienskies.core.game.ships.ShipObject
 
 interface BlockEntityShipProvider : ShipProvider {
-    override var ship: Ship?
-
     /**
-     * Gets called on block entity creation and relocation
-     *  the actual new [BlockPos], [Ship] and [Level] shall be assigned
-     *  on this [BlockEntity] after this call
-     * @param newPos
-     * @param newLevel
-     * @param newShip
+     * Ship for the block entity, it will be set on creation
      */
-    fun onShipChange(newPos: BlockPos, newLevel: Level, newShip: ShipObject) {}
+    override var ship: Ship?
 }
