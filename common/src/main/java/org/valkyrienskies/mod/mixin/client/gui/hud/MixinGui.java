cb5ce17754ddebe20dc9e3ae1aec84267b55d25f
Rubydesic <rubydesic@gmail.com>
2022-07-28 23:01:53 -0400

    Remove VSOptions

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/gui/hud/MixinGui.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/gui/hud/MixinGui.java
@@ -17,7 +17,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.valkyrienskies.core.game.VSOptions;
 import org.valkyrienskies.core.networking.VSNetworking;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.config.VSGameConfig;
