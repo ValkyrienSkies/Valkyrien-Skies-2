639478a15e03bda86f862452252dce5b599316c7
Rubydesic <rubydesic@gmail.com>
2022-07-31 21:25:18 -0400

    pause physics when game pauses, closes #112

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/MixinMinecraft.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/MixinMinecraft.java
@@ -5,12 +5,15 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,13 +22,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.core.game.ships.QueryableShipDataImpl;
 import org.valkyrienskies.core.game.ships.ShipObjectClientWorld;
 import org.valkyrienskies.core.networking.VSNetworking;
+import org.valkyrienskies.core.pipelines.VSPipeline;
 import org.valkyrienskies.mod.common.IShipObjectWorldClientCreator;
 import org.valkyrienskies.mod.common.IShipObjectWorldClientProvider;
+import org.valkyrienskies.mod.common.IShipObjectWorldServerProvider;
 import org.valkyrienskies.mod.common.PlayerUtil;
 import org.valkyrienskies.mod.mixinducks.client.MinecraftDuck;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraft implements MinecraftDuck, IShipObjectWorldClientProvider, IShipObjectWorldClientCreator {
+public abstract class MixinMinecraft
+    implements MinecraftDuck, IShipObjectWorldClientProvider, IShipObjectWorldClientCreator {
+
+    @Shadow
+    private boolean pause;
+
+    @Shadow
+    @Nullable
+    public abstract IntegratedServer getSingleplayerServer();
 
     @Unique
     private HitResult originalCrosshairTarget;
@@ -61,6 +74,20 @@ public class MixinMinecraft implements MinecraftDuck, IShipObjectWorldClientProv
         // Tick the ship world
         if (shipObjectWorld != null) {
             shipObjectWorld.tickShips();
+        }
+    }
+
+    @Inject(
+        method = "runTick",
+        at = @At(value = "TAIL")
+    )
+    public void setGamePause(final boolean pauseOnly, final CallbackInfo ci) {
+        final IShipObjectWorldServerProvider provider = (IShipObjectWorldServerProvider) getSingleplayerServer();
+        if (provider != null) {
+            final VSPipeline pipeline = provider.getVsPipeline();
+            if (pipeline != null) {
+                pipeline.setArePhysicsRunning(!this.pause);
+            }
         }
     }
 
