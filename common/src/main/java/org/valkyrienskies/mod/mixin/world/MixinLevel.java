f1c1faec1e660fe1cab81ce834441263006fdd52
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-09-10 22:58:55 +0200

    No AABB freezes anymore

--- common/src/main/java/org/valkyrienskies/mod/mixin/world/MixinLevel.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/world/MixinLevel.java
@@ -1,21 +1,34 @@
 package org.valkyrienskies.mod.mixin.world;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.AABB;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.game.ships.ShipObjectServer;
 import org.valkyrienskies.mod.api.ShipBlockEntity;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 @Mixin(Level.class)
 public class MixinLevel {
+    @Unique
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Shadow
     @Final
@@ -31,4 +44,68 @@ public class MixinLevel {
         }
     }
 
+    @Inject(
+        method = "getEntities(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;",
+        at = @At("HEAD"), cancellable = true)
+    public void checkAABB1(@Nullable final Entity entity,
+        final AABB area,
+        @Nullable final Predicate<? super Entity> predicate,
+        final CallbackInfoReturnable<List<Entity>> cir) {
+
+        if (isCollisionBoxToBig(area)) {
+            LOGGER.error("Collision box is too big! " + area + " returning empty list! this might break things");
+            cir.setReturnValue(Collections.EMPTY_LIST);
+            cir.cancel();
+        }
+    }
+
+    @Inject(
+        method = "getLoadedEntitiesOfClass",
+        at = @At("HEAD"), cancellable = true)
+    public <T extends Entity> void checkAABB2(final Class<? extends T> class_, final AABB area,
+        @Nullable final Predicate<? super T> predicate,
+        final CallbackInfoReturnable<List<T>> cir) {
+
+        if (isCollisionBoxToBig(area)) {
+            LOGGER.error("Collision box is too big! " + area + " returning empty list! this might break things");
+            cir.setReturnValue(Collections.EMPTY_LIST);
+            cir.cancel();
+        }
+    }
+
+    @Inject(
+        method = "getEntitiesOfClass",
+        at = @At("HEAD"), cancellable = true)
+    public <T extends Entity> void checkAABB3(
+        final Class<? extends T> clazz,
+        final AABB area,
+        @Nullable final Predicate<? super T> filter,
+        final CallbackInfoReturnable<List<T>> cir) {
+
+        if (isCollisionBoxToBig(area)) {
+            LOGGER.error("Collision box is too big! " + area + " returning empty list! this might break things");
+            cir.setReturnValue(Collections.EMPTY_LIST);
+            cir.cancel();
+        }
+    }
+
+    @Inject(
+        method = "getEntities(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;)Ljava/util/List;",
+        at = @At("HEAD"), cancellable = true)
+    public <T extends Entity> void checkAABB4(@Nullable final EntityType<T> entityType,
+        final AABB area,
+        final Predicate<? super T> predicate,
+        final CallbackInfoReturnable<List<T>> cir) {
+
+        if (isCollisionBoxToBig(area)) {
+            LOGGER.error("Collision box is too big! " + area + " returning empty list! this might break things");
+            cir.setReturnValue(Collections.EMPTY_LIST);
+            cir.cancel();
+        }
+    }
+
+    @Unique
+    public boolean isCollisionBoxToBig(final AABB aabb) {
+        return aabb.getXsize() > 1000 || aabb.getYsize() > 1000 || aabb.getZsize() > 1000;
+    }
 }
