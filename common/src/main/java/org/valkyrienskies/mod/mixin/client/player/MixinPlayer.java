107f9751ed2881d3b4b723351959bff1322339b6
Rubydesic <rubydesic@gmail.com>
2022-10-29 17:02:48 -0400

    Fix invalid move packet bug, bump dependency downloader version

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/player/MixinPlayer.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/player/MixinPlayer.java
@@ -1,5 +1,8 @@
 package org.valkyrienskies.mod.mixin.client.player;
 
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -8,7 +11,7 @@ import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.entity.ShipMountingEntity;
 
 @Mixin(Player.class)
 public abstract class MixinPlayer extends LivingEntity {
@@ -20,11 +23,15 @@ public abstract class MixinPlayer extends LivingEntity {
     @Redirect(
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/phys/AABB;minmax(Lnet/minecraft/world/phys/AABB;)Lnet/minecraft/world/phys/AABB;"
+            target = "Lnet/minecraft/world/level/Level;getEntities(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;)Ljava/util/List;"
         ),
         method = "aiStep"
     )
-    private AABB redirectMinMaxWithVehicleAABB(final AABB instance, final AABB other) {
-        return instance.minmax(VSGameUtilsKt.transformAabbToWorld(level, other));
+    private List<Entity> redirectAiStep(final Level instance, final Entity entity, final AABB aabb) {
+        if (this.getVehicle() instanceof ShipMountingEntity) {
+            return new ArrayList<>();
+        }
+
+        return instance.getEntities(entity, aabb);
     }
 }
