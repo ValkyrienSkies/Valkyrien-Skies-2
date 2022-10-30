521818a75bd566dc84eb54c6606d6dfb8de29a19
Rubydesic <rubydesic@gmail.com>
2022-10-30 17:19:06 -0400

    Remove headless checks from auto updater invoking code

--- fabric/src/main/java/org/valkyrienskies/mod/fabric/AutoDependenciesFabric.java
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/AutoDependenciesFabric.java
@@ -9,10 +9,7 @@ public class AutoDependenciesFabric {
 
     public static void runUpdater() {
         final boolean isServer = FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER;
-        if (!isServer) {
-            System.setProperty("java.awt.headless", "false");
-        }
-
+        
         ValkyrienDependencyDownloader.start(
             FabricLoader.getInstance().getGameDir().resolve("mods"),
             // remove any dependencies that are already loaded by fabric
