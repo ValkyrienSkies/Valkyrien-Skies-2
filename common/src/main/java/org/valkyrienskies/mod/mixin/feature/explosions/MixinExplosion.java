39d294a4f684bee7e744c3f9596a573613b6466a
Grant Voelker <advanceseven@gmail.com>
2022-10-31 19:41:55 -0400

    Explosion push force for ships (#177)
    * - Game tick force applier for all ships that can apply forces from anywhere in gametick
     - Explosions push ships relative to how much they were hit and distance to explosion
    
    * - Fix fabric version
     - Clean up explode force code
     - Add config for explosion force
     - Make force multiply by radius, roughly equivalent to blast force
    
    * - Make explode force code a private method
     - Remove unused finalizeExplosion mixin
    
    * - Added a comment explaining necessity of ClipContextMixin

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/explosions/MixinExplosion.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/explosions/MixinExplosion.java
@@ -2,10 +2,17 @@ package org.valkyrienskies.mod.mixin.feature.explosions;
 
 import java.util.Collections;
 import java.util.List;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult.Type;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3d;
+import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -15,7 +22,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ServerShip;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.config.VSGameConfig;
+import org.valkyrienskies.mod.common.util.GameTickForceApplier;
+import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 
 @Mixin(Explosion.class)
 public abstract class MixinExplosion {
@@ -46,9 +57,58 @@ public abstract class MixinExplosion {
     @Shadow
     public abstract void explode();
 
+    @Unique
+    private void doExplodeForce() {
+        // Custom forces
+        final Vector3d originPos = new Vector3d(this.x, this.y, this.z);
+        final BlockPos explodePos = new BlockPos(originPos.x(), originPos.y(), originPos.z());
+        final int radius = (int) Math.ceil(this.radius);
+        for (int x = radius; x >= -radius; x--) {
+            for (int y = radius; y >= -radius; y--) {
+                for (int z = radius; z >= -radius; z--) {
+                    final BlockHitResult result = level.clip(
+                        new ClipContext(Vec3.atCenterOf(explodePos),
+                            Vec3.atCenterOf(explodePos.offset(x, y, z)),
+                            ClipContext.Block.COLLIDER,
+                            ClipContext.Fluid.NONE, null));
+                    if (result.getType() == Type.BLOCK) {
+                        final BlockPos blockPos = result.getBlockPos();
+                        final ServerShip ship =
+                            (ServerShip) VSGameUtilsKt.getShipObjectManagingPos(this.level, blockPos);
+                        if (ship != null) {
+                            final Vector3d forceVector =
+                                VectorConversionsMCKt.toJOML(
+                                    Vec3.atCenterOf(explodePos)); //Start at center position
+                            final Double distanceMult = Math.max(0.5, 1.0 - (this.radius /
+                                forceVector.distance(VectorConversionsMCKt.toJOML(Vec3.atCenterOf(blockPos)))));
+                            final Double powerMult = Math.max(0.1, this.radius / 4); //TNT blast radius = 4
+
+                            forceVector.sub(VectorConversionsMCKt.toJOML(
+                                Vec3.atCenterOf(blockPos))); //Subtract hit block pos to get direction
+                            forceVector.normalize();
+                            forceVector.mul(-1 *
+                                VSGameConfig.SERVER.getExplosionBlastForce()); //Multiply by blast force at center position. Negative because of how we got the direction.
+                            forceVector.mul(distanceMult); //Multiply by distance falloff
+                            forceVector.mul(powerMult); //Multiply by radius, roughly equivalent to power
+
+                            final GameTickForceApplier forceApplier =
+                                ship.getAttachment(GameTickForceApplier.class);
+                            final Vector3dc shipCoords = ship.getShipTransform().getShipPositionInShipCoordinates();
+                            if (forceVector.isFinite()) {
+                                forceApplier.applyInvariantForceToPos(forceVector,
+                                    VectorConversionsMCKt.toJOML(Vec3.atCenterOf(blockPos)).sub(shipCoords));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     @Inject(at = @At("TAIL"), method = "explode")
     private void afterExplode(final CallbackInfo ci) {
         if (isModifyingExplosion) {
+            doExplodeForce();
             return;
         }
 
@@ -87,4 +147,5 @@ public abstract class MixinExplosion {
             return instance.getEntities(entity, aabb);
         }
     }
+
 }
---
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/feature/explosions/ClipContextMixin.java
@@ -0,0 +1,31 @@
+package org.valkyrienskies.mod.fabric.mixin.feature.explosions;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClipContext.class)
+public class ClipContextMixin {
+    @Shadow
+    @Final
+    private CollisionContext collisionContext;
+
+    //In features like explosion push force, there is not always a relevant entity for a clip to use.
+    //CollisionContext.of in vanilla requires a non-null entity. This mixin makes parity with the Forge implementation of CollisionContext.
+    @Redirect(method = "<init>",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/phys/shapes/CollisionContext;of(Lnet/minecraft/world/entity/Entity;)Lnet/minecraft/world/phys/shapes/CollisionContext;"
+        ))
+    public CollisionContext collisionContextRedirect(final Entity entity) {
+        if (entity == null) {
+            return CollisionContext.empty();
+        }
+        return CollisionContext.of(entity);
+    }
+}
