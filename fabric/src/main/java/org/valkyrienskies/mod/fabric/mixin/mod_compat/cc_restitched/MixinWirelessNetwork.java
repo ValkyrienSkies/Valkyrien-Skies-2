ca07f1daea2bb506a243268ab0267bf9d495c156
TechTastic <74630543+TechTastic@users.noreply.github.com>
2022-11-07 11:05:17 -0600

    It doesnt work...

--- fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/mod_compat/cc_restitched/MixinWirelessNetwork.java
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/mod_compat/cc_restitched/MixinWirelessNetwork.java
@@ -5,13 +5,11 @@ import dan200.computercraft.api.network.IPacketSender;
 import dan200.computercraft.api.network.Packet;
 import dan200.computercraft.shared.peripheral.modem.wireless.WirelessNetwork;
 import net.minecraft.world.phys.Vec3;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
@@ -21,29 +19,19 @@ public class MixinWirelessNetwork {
     private static IPacketReceiver shipReceiver;
     private static IPacketSender shipSender;
 
-    /*@Redirect(method = "tryTransmit",
-        at = @At(value = "FIELD",
-            target = "Ldan200/computercraft/shared/peripheral/modem/wireless/WirelessNetwork.tryTransmit();sqDistance",
-            opcode = Opcodes.PUTFIELD)
-    )
-    private void ValkyrienIsles2$sqDistance(double owner, double sqDistance) {
-        Vec3 recPos = shipReceiver.getPosition();
-        Vec3 senPos = shipSender.getPosition();
-        owner = VSGameUtilsKt.squaredDistanceBetweenInclShips(shipReceiver.getWorld(), recPos.x, recPos.y, recPos.z, senPos.x, senPos.y, senPos.z);
-    }*/
-
     @ModifyVariable(method = "tryTransmit",
-        at = @At(value = "INVOKE_ASSIGN", target = "Ldan200/computercraft/shared/peripheral/modem/wireless/WirelessNetwork;tryTransmit/distanceToSqr()D"),
-        index = 1
+        at = @At(value = "STORE"),
+        name = "distanceSq",
+        remap = false
     )
-    public double ValkyrienIsles2$sqDistance(double original) {
+    private static double ValkyrienIsles2$distanceSq(double original) {
         Vec3 recPos = shipReceiver.getPosition();
         Vec3 senPos = shipSender.getPosition();
         return VSGameUtilsKt.squaredDistanceBetweenInclShips(shipReceiver.getWorld(), recPos.x, recPos.y, recPos.z, senPos.x, senPos.y, senPos.z);
     }
 
-    @Inject(at = @At("HEAD"), method = "tryTransmit")
-    public static void ValkyrienIsles2$tryTransmit(IPacketReceiver receiver, Packet packet, double range, boolean interdimensional, CallbackInfo ci) {
+    @Inject(at = @At("HEAD"), method = "tryTransmit", remap = false)
+    private static void ValkyrienIsles2$tryTransmit(IPacketReceiver receiver, Packet packet, double range, boolean interdimensional, CallbackInfo ci) {
         shipReceiver = receiver;
         shipSender = packet.getSender();
     }
