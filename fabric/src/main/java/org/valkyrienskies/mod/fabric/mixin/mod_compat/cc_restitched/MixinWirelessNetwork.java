4e65378dbe512b8ed305cb0ca36b43fdfefd3c83
TechTastic <74630543+TechTastic@users.noreply.github.com>
2022-11-08 08:47:34 -0600

    Before finding the other distance checks

--- fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/mod_compat/cc_restitched/MixinWirelessNetwork.java
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/mod_compat/cc_restitched/MixinWirelessNetwork.java
@@ -5,6 +5,7 @@ import dan200.computercraft.api.network.IPacketSender;
 import dan200.computercraft.api.network.Packet;
 import dan200.computercraft.shared.peripheral.modem.wireless.WirelessNetwork;
 import net.minecraft.world.phys.Vec3;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +13,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod;
+import org.valkyrienskies.mod.fabric.common.ValkyrienSkiesModFabric;
 
 @Pseudo
 @Mixin(WirelessNetwork.class)
@@ -24,14 +27,22 @@ public class MixinWirelessNetwork {
         name = "distanceSq",
         remap = false
     )
-    private static double ValkyrienIsles2$distanceSq(double original) {
+    private static double ValkyrienSkies2$distanceSq(double original) {
+        System.err.println("This Mixin was called!");
+
         Vec3 recPos = shipReceiver.getPosition();
         Vec3 senPos = shipSender.getPosition();
-        return VSGameUtilsKt.squaredDistanceBetweenInclShips(shipReceiver.getWorld(), recPos.x, recPos.y, recPos.z, senPos.x, senPos.y, senPos.z);
+        double distance = VSGameUtilsKt.squaredDistanceBetweenInclShips(shipReceiver.getWorld(), recPos.x, recPos.y, recPos.z, senPos.x, senPos.y, senPos.z);
+
+        System.err.println("Receiver: " + recPos.toString());
+        System.err.println("Sneder: " + senPos.toString());
+        System.err.println("Distance: " + distance);
+
+        return distance;
     }
 
     @Inject(at = @At("HEAD"), method = "tryTransmit", remap = false)
-    private static void ValkyrienIsles2$tryTransmit(IPacketReceiver receiver, Packet packet, double range, boolean interdimensional, CallbackInfo ci) {
+    private static void ValkyrienSkies2$tryTransmit(IPacketReceiver receiver, Packet packet, double range, boolean interdimensional, CallbackInfo ci) {
         shipReceiver = receiver;
         shipSender = packet.getSender();
     }
