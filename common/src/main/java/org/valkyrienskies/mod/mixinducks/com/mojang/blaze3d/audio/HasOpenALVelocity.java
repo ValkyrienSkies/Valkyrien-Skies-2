43a5ec6061bbc125e227ea5a384e33e856144950
Rubydesic <rubydesic@gmail.com>
2022-11-29 13:15:28 -0500

    fix doppler shift code

--- common/src/main/java/org/valkyrienskies/mod/mixinducks/com/mojang/blaze3d/audio/HasOpenALVelocity.java
+++ common/src/main/java/org/valkyrienskies/mod/mixinducks/com/mojang/blaze3d/audio/HasOpenALVelocity.java
@@ -2,6 +2,6 @@ package org.valkyrienskies.mod.mixinducks.com.mojang.blaze3d.audio;
 
 import org.joml.Vector3dc;
 
-public interface ChannelDuck {
+public interface HasOpenALVelocity {
     void setVelocity(final Vector3dc velocity);
 }
