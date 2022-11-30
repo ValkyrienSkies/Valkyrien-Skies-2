ffa8279bd6292df9b051a13083c6efbba2e66319
StewStrong <stew.strong.420@gmail.com>
2022-11-30 03:11:35 -0700

    Fixed player falling through world when loading game mounted to a ship

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/FEATURE.md
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/FEATURE.md
@@ -11,6 +11,10 @@ in the ship.
     * This is relevant for detecting if a entity is going out the ship's bounds.
 * `MixinEntity#positionRider` Handles mounted entities on other entities.
     * This is relevant for allowing world entities mounting shipyard entities.
+* `MixinEntity#preventSavingVehiclePosAsOurPos` Prevents the game from saving
+  the vehicle position as the entities' position.
+    * This fixes players falling through the world when loading if they were
+      mounted to a ship entity when the game saved.
 * `MixinEntityRenderDispatcher#render` Handles rendering of entities.
     * This is relevant for rendering entities on ships.
 * `MixinEntityRenderDispatcher#shouldRender` Handles rendering of entities.
--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/MixinEntity.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/MixinEntity.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.valkyrienskies.core.api.Ship;
+import org.valkyrienskies.core.game.ChunkAllocator;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.entity.handling.VSEntityManager;
 
@@ -20,7 +21,7 @@ public abstract class MixinEntity {
      */
     @Redirect(method = "positionRider(Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "INVOKE",
         target = "Lnet/minecraft/world/entity/Entity;positionRider(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/entity/Entity$MoveFunction;)V"))
-    public void positionRider(final Entity instance, final Entity passengerI, final Entity.MoveFunction callback) {
+    private void positionRider(final Entity instance, final Entity passengerI, final Entity.MoveFunction callback) {
         this.positionRider(passengerI,
             (passenger, x, y, z) -> VSEntityManager.INSTANCE.getHandler(passenger.getType())
                 .positionSetFromVehicle(passenger, Entity.class.cast(this), x, y, z));
@@ -51,7 +52,7 @@ public abstract class MixinEntity {
      */
     @Redirect(method = "setLocationFromBoundingbox", at = @At(value = "INVOKE",
         target = "Lnet/minecraft/world/entity/Entity;setPosRaw(DDD)V"))
-    public void setPosHandling2(final Entity instance, final double x, final double y, final double z) {
+    private void setPosHandling2(final Entity instance, final double x, final double y, final double z) {
         final Vector3d pos = new Vector3d(x, y, z);
         final Ship ship;
 
@@ -59,6 +60,29 @@ public abstract class MixinEntity {
             VSEntityManager.INSTANCE.getHandler(instance.getType()).onEntityMove(instance, ship, pos);
         } else {
             instance.setPosRaw(x, y, z);
+        }
+    }
+
+    /**
+     * Prevent [saveWithoutId] from saving the vehicle's position as passenger's position when that vehicle is in the
+     * shipyard.
+     *
+     * This fixes players falling through the world when they load into the world and were mounting an entity on a ship.
+     */
+    @Redirect(method = "saveWithoutId", at = @At(value = "FIELD",
+        target = "Lnet/minecraft/world/entity/Entity;vehicle:Lnet/minecraft/world/entity/Entity;"))
+    private Entity preventSavingVehiclePosAsOurPos(final Entity originalVehicle) {
+        // Only check this if [originalVehicle] != null
+        if (originalVehicle == null) return null;
+
+        final int vehicleChunkX = ((int) originalVehicle.position().x()) >> 4;
+        final int vehicleChunkZ = ((int) originalVehicle.position().z()) >> 4;
+        // Don't apply the ship render transform if the player is in the shipyard
+        final boolean isVehicleInShipyard = ChunkAllocator.isChunkInShipyard(vehicleChunkX, vehicleChunkZ);
+        if (isVehicleInShipyard) {
+            return null;
+        } else {
+            return originalVehicle;
         }
     }
 
