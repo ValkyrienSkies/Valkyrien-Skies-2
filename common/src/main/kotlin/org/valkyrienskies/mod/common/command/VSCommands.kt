be842bc487e0a49830d923aa000aa73db7b24ab2
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-12-01 21:38:56 +0100

    ShipSelector Reco & Parsing WIP

--- common/src/main/kotlin/org/valkyrienskies/mod/common/command/VSCommands.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/command/VSCommands.kt
@@ -1,15 +1,19 @@
 package org.valkyrienskies.mod.common.command
 
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.arguments.FloatArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import net.minecraft.commands.CommandRuntimeException
 import net.minecraft.commands.CommandSourceStack
-import net.minecraft.commands.Commands.argument
-import net.minecraft.commands.Commands.literal
 import org.valkyrienskies.core.api.ships.ServerShip
 import org.valkyrienskies.core.api.world.ServerShipWorld
 import org.valkyrienskies.core.api.world.ShipWorld
+import org.valkyrienskies.core.util.x
+import org.valkyrienskies.core.util.y
+import org.valkyrienskies.core.util.z
 import org.valkyrienskies.mod.common.vsCore
 import org.valkyrienskies.mod.mixinducks.feature.command.VSCommandSource
 import org.valkyrienskies.mod.util.logger
@@ -17,10 +21,18 @@ import org.valkyrienskies.mod.util.logger
 object VSCommands {
     private val LOGGER by logger()
 
+    private fun literal(name: String) =
+        LiteralArgumentBuilder.literal<VSCommandSource>(name)
+
+    private fun <T> argument(name: String, type: ArgumentType<T>) =
+        RequiredArgumentBuilder.argument<VSCommandSource, T>(name, type)
+
     fun registerServerCommands(dispatcher: CommandDispatcher<CommandSourceStack>) {
+        dispatcher as CommandDispatcher<VSCommandSource>
+
         dispatcher.register(
             literal("vs")
-                .then(literal("delete").then(argument("ships", ShipArgument).executes {
+                .then(literal("delete").then(argument("ships", ShipArgument.ships()).executes {
                     try {
                         val r = ShipArgument.getShips(it, "ships").toList() as List<ServerShip>
                         vsCore.deleteShips(it.source.shipWorld as ServerShipWorld, r)
@@ -35,7 +47,7 @@ object VSCommands {
                 // Single ship commands
                 .then(
                     literal("ship").then(
-                        argument("ship", ShipArgument)
+                        argument("ship", ShipArgument.ships())
 
                             // Delete a ship
                             .then(literal("delete").executes {
@@ -87,6 +99,16 @@ object VSCommands {
                             )
                     )
                 )
+        )
+
+        dispatcher.root.children.first { it.name == "teleport" }.addChild(
+            argument("ship", ShipArgument.selectorOnly()).executes {
+                val ship = ShipArgument.getShip(it, "ship")
+                val source = it.source as CommandSourceStack
+                val shipPos = ship.transform.positionInWorld
+
+                source.entity?.let { it.teleportTo(shipPos.x, shipPos.y, shipPos.z); 1 } ?: 0
+            }.build()
         )
     }
 
