e8a775a4391b0a5482114227edc713fbd4ff7117
Rubydesic <rubydesic@gmail.com>
2022-12-05 20:08:57 -0500

    fix: quark compatibility

--- common/src/main/kotlin/org/valkyrienskies/mod/common/PlayerUtil.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/PlayerUtil.kt
@@ -1,17 +1,66 @@
 package org.valkyrienskies.mod.common
 
+import com.google.common.collect.MapMaker
 import net.minecraft.core.BlockPos
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
+import net.minecraft.world.phys.Vec3
 import org.valkyrienskies.core.game.ChunkAllocator
 import org.valkyrienskies.core.game.ships.ShipObject
 import org.valkyrienskies.mod.common.util.toJOML
 import org.valkyrienskies.mod.common.util.toMinecraft
 import org.valkyrienskies.mod.mixin.accessors.entity.EntityAccessor
+import java.util.concurrent.ConcurrentMap
 import kotlin.math.atan2
 import kotlin.math.sqrt
 
 object PlayerUtil {
+
+    private val prevPosInfo: ConcurrentMap<Player, TempPlayerPosInfo> = MapMaker().weakKeys().makeMap()
+
+    private data class TempPlayerPosInfo(val yaw: Float, val headYaw: Float, val pitch: Float, val pos: Vec3)
+
+    @JvmStatic
+    fun transformPlayerTemporarily(player: Player, world: Level, blockInShip: BlockPos) =
+        transformPlayerTemporarily(player, world.getShipObjectManagingPos(blockInShip))
+
+    @JvmStatic
+    fun transformPlayerTemporarily(player: Player, ship: ShipObject?) {
+        if (ChunkAllocator.isBlockInShipyard(player.x, player.y, player.z)) {
+            // player is already in shipyard
+            return
+        }
+
+        prevPosInfo[player] = TempPlayerPosInfo(player.yRot, player.yHeadRot, player.xRot, player.position())
+
+        if (ship != null) {
+            val shipMatrix = ship.shipTransform.worldToShipMatrix
+            val direction = shipMatrix.transformDirection(
+                player.lookAngle.toJOML()
+            )
+            val position = shipMatrix.transformPosition(
+                player.position().toJOML()
+            )
+            val yaw = -atan2(direction.x, direction.z)
+            val pitch = -atan2(direction.y, sqrt((direction.x * direction.x) + (direction.z * direction.z)))
+            player.yRot = (yaw * (180 / Math.PI)).toFloat()
+            player.yHeadRot = player.yRot
+            player.xRot = (pitch * (180 / Math.PI)).toFloat()
+            (player as EntityAccessor).setPosNoUpdates(position.toMinecraft())
+        }
+    }
+
+    @JvmStatic
+    fun untransformPlayer(player: Player) {
+        val info = prevPosInfo.remove(player) ?: return
+        player.xRot = info.pitch
+
+        player.yRot = info.yaw
+        player.yHeadRot = info.headYaw
+
+        (player as EntityAccessor).setPosNoUpdates(info.pos)
+    }
+
     /**
      * Updates [player] to 'live' in ship space for everything executed in the inside lambda
      * is used for emulating the environment when you interact with a block, [blockInShip]
@@ -26,42 +75,12 @@ object PlayerUtil {
      */
     @JvmStatic
     fun <T> transformPlayerTemporarily(player: Player, ship: ShipObject?, inside: () -> T): T {
-        if (ChunkAllocator.isBlockInShipyard(player.x, player.y, player.z)) {
-            // player is already in shipyard
-            return inside()
-        }
-
-        val tmpYaw = player.yRot
-        val tmpHeadYaw = player.yHeadRot
-        val tmpPitch = player.xRot
-        val tmpPos = player.position()
-
-        if (ship != null) {
-            val shipMatrix = ship.shipData.shipTransform
-                .worldToShipMatrix
-            val direction = shipMatrix.transformDirection(
-                player.lookAngle.toJOML()
-            )
-            val position = shipMatrix.transformPosition(
-                player.position().toJOML()
-            )
-            val yaw = -atan2(direction.x, direction.z)
-            val pitch = -atan2(direction.y, sqrt((direction.x * direction.x) + (direction.z * direction.z)))
-            player.yRot = (yaw * (180 / Math.PI)).toFloat()
-            player.yHeadRot = player.yRot
-            player.xRot = (pitch * (180 / Math.PI)).toFloat()
-            (player as EntityAccessor).setPosNoUpdates(position.toMinecraft())
-        }
+        transformPlayerTemporarily(player, ship)
 
         try {
             return inside()
         } finally {
-            player.xRot = tmpPitch
-
-            player.yRot = tmpYaw
-            player.yHeadRot = tmpHeadYaw
-
-            (player as EntityAccessor).setPosNoUpdates(tmpPos)
+            untransformPlayer(player)
         }
     }
 }
