e126fbc81387b97860e7465c0c140bcb977700a6
Rubydesic <rubydesic@gmail.com>
2022-12-06 13:58:25 -0500

    fix: reset openal player velocity every tick

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/sound/client/MixinSoundEngine.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/sound/client/MixinSoundEngine.java
@@ -9,6 +9,7 @@ import net.minecraft.client.sounds.ChannelAccess;
 import net.minecraft.client.sounds.SoundEngine;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3d;
 import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -67,6 +68,8 @@ public abstract class MixinSoundEngine {
     private void injectListenerVelocity(final Listener listener, final Vec3 position) {
         final Player player = Minecraft.getInstance().player;
         final ClientLevel level = Minecraft.getInstance().level;
+        ((HasOpenALVelocity) listener).setVelocity(new Vector3d());
+        
         if (level != null && player != null) {
             final ClientShip mounted = VSGameUtilsKt.getShipObjectEntityMountedTo(level, player);
             if (mounted != null) {
