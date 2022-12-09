dbfefdf6444e856602802885eb8967206065ae25
Rubydesic <rubydesic@gmail.com>
2022-12-08 21:38:28 -0500

    fix: improve collision box error logging

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/get_entities/MixinLevel.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/get_entities/MixinLevel.java
@@ -1,6 +1,6 @@
 package org.valkyrienskies.mod.mixin.feature.get_entities;
 
-import com.mojang.logging.LogUtils;
+import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,20 +8,25 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.entity.EntityTypeTest;
 import net.minecraft.world.phys.AABB;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.valkyrienskies.core.impl.util.RateLimiter;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 @Mixin(Level.class)
 public class MixinLevel {
     @Unique
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger("VS2 Get Entities Mixin");
+
+    @Unique
+    private static final RateLimiter LIMITER = new RateLimiter(Duration.ofSeconds(5));
 
     @Unique
     private static boolean isCollisionBoxToBig(final AABB aabb) {
@@ -55,7 +60,9 @@ public class MixinLevel {
         final Predicate<? super T> predicate, final CallbackInfoReturnable<List<T>> cir) {
 
         if (isCollisionBoxToBig(area)) {
-            LOGGER.error("Collision box is too big! " + area + " returning empty list! this might break things");
+            LIMITER.maybeRun(() ->
+                LOGGER.error(new Exception(
+                    "Collision box is too big! " + area + " returning empty list! this might break things")));
             cir.setReturnValue(Collections.emptyList());
             cir.cancel();
         }
@@ -70,7 +77,9 @@ public class MixinLevel {
         final Predicate<? super Entity> predicate, final CallbackInfoReturnable<List<Entity>> cir) {
 
         if (isCollisionBoxToBig(area)) {
-            LOGGER.error("Collision box is too big! " + area + " returning empty list! this might break things");
+            LIMITER.maybeRun(() ->
+                LOGGER.error(new Exception(
+                    "Collision box is too big! " + area + " returning empty list! this might break things")));
             cir.setReturnValue(Collections.emptyList());
             cir.cancel();
         }
