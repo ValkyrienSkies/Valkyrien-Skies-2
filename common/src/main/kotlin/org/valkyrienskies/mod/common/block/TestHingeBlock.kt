cd5fc6093d34461fafc1193fcdc9342c4bb1f91c
StewStrong <stew.strong.420@gmail.com>
2022-12-16 23:48:10 -0700

    Hinge god

--- common/src/main/kotlin/org/valkyrienskies/mod/common/block/TestHingeBlock.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/block/TestHingeBlock.kt
@@ -2,6 +2,7 @@ package org.valkyrienskies.mod.common.block
 
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
@@ -9,6 +10,7 @@ import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.minecraft.world.level.block.SoundType
@@ -21,7 +23,19 @@ import net.minecraft.world.level.material.Material
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
+import org.joml.AxisAngle4d
+import org.joml.Quaterniond
+import org.joml.Vector3d
+import org.joml.Vector3dc
+import org.valkyrienskies.core.apigame.constraints.VSAttachmentConstraint
+import org.valkyrienskies.core.apigame.constraints.VSHingeOrientationConstraint
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod
 import org.valkyrienskies.mod.common.blockentity.TestHingeBlockEntity
+import org.valkyrienskies.mod.common.dimensionId
+import org.valkyrienskies.mod.common.getShipManagingPos
+import org.valkyrienskies.mod.common.shipObjectWorld
+import org.valkyrienskies.mod.common.util.toJOML
+import kotlin.math.roundToInt
 
 object TestHingeBlock :
     HorizontalDirectionalBlock(
@@ -56,8 +70,68 @@ object TestHingeBlock :
         hand: InteractionHand,
         blockHitResult: BlockHitResult
     ): InteractionResult {
+        println("test")
         if (level.isClientSide) return InteractionResult.SUCCESS
 
+        val blockEntity = level.getBlockEntity(pos, ValkyrienSkiesMod.TEST_HINGE_BLOCK_ENTITY_TYPE)
+
+        if (blockEntity.isPresent) {
+            if (blockEntity.get().otherHingePos == null) {
+                val ship = (level as ServerLevel).shipObjectWorld.createNewShipAtBlock(
+                    pos.offset(0, 1, 0).toJOML(), false, 1.0, level.dimensionId
+                )
+                val shipCenterPos = BlockPos(
+                    (ship.transform.positionInShip.x() - 0.5).roundToInt(),
+                    (ship.transform.positionInShip.y() - 0.5).roundToInt(),
+                    (ship.transform.positionInShip.z() - 0.5).roundToInt()
+                )
+                // println("shipCenterPos is $shipCenterPos")
+                level.setBlockAndUpdate(shipCenterPos, Blocks.IRON_BLOCK.defaultBlockState())
+                blockEntity.get().otherHingePos = shipCenterPos
+
+                val shipThisIsIn = level.getShipManagingPos(pos)
+
+                val shipId0 = shipThisIsIn?.id ?: level.shipObjectWorld.dimensionToGroundBodyIdImmutable[level.dimensionId]!!
+                val shipId1 = ship.id
+
+                // Attachment constraint
+                run {
+                    val attachmentCompliance = 1e-8
+                    val attachmentLocalPos0: Vector3dc = Vector3d(pos.x + .5, pos.y + 1.5, pos.z + .5)
+                    val attachmentLocalPos1: Vector3dc =
+                        Vector3d(shipCenterPos.x.toDouble(), shipCenterPos.y.toDouble(), shipCenterPos.z.toDouble())
+                    val attachmentMaxForce = 1e8
+                    val attachmentFixedDistance = 0.0
+                    val attachmentConstraint = VSAttachmentConstraint(
+                        shipId0, shipId1, attachmentCompliance, attachmentLocalPos0, attachmentLocalPos1,
+                        attachmentMaxForce, attachmentFixedDistance
+                    )
+                    blockEntity.get().constraintId = level.shipObjectWorld.createNewConstraint(attachmentConstraint)
+                }
+
+                // Hinge orientation constraint
+                run {
+                    val hingeMaxTorque = 1e10
+                    // Hinge constraints will attempt to align the X-axes of both bodies, so to align the Y axis we
+                    // apply this rotation to the X-axis
+                    // TODO: Logically this should be Quaterniond(AxisAngle4d(Math.toRadians(90.0), 0.0, 0.0, 1.0))
+                    //       instead, maybe the physics api is broken?
+                    val hingeOrientation = Quaterniond(AxisAngle4d(Math.toRadians(90.0), 1.0, 0.0, 0.0))
+                    val hingeConstraint = VSHingeOrientationConstraint(
+                        shipId0, shipId1, 1e-10, hingeOrientation, hingeOrientation, hingeMaxTorque
+                    )
+                    blockEntity.get().constraintId = level.shipObjectWorld.createNewConstraint(hingeConstraint)
+                }
+
+                // Pos damping constraint
+                // val posDampingConstraint = VSPosDampingConstraint(shipId0, shipId1, compliance, localPos0, localPos1, maxForce, 1e-3)
+                // blockEntity.get().constraintId = level.shipObjectWorld.createNewConstraint(posDampingConstraint)
+
+                // Rot damping constraint
+                // val rotDampingConstraint = VSRotDampingConstraint(shipId0, shipId1, compliance, Quaterniond(), Quaterniond(), maxTorque, 1e-2)
+                // blockEntity.get().constraintId = level.shipObjectWorld.createNewConstraint(rotDampingConstraint)
+            }
+        }
         return InteractionResult.CONSUME
     }
 
