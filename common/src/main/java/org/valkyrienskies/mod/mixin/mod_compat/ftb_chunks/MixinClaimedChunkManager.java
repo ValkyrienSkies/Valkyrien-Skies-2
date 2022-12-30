0632029172ff50041fa9b040dcc50c28f49c04cb
TechTastic <74630543+TechTastic@users.noreply.github.com>
2022-12-29 20:14:18 -0600

    Fixed Crash
    Edited MixinClaimedChunkManager to change the position passed to the newly created ChunkDimPos instance for getChunk

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/ftb_chunks/MixinClaimedChunkManager.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/ftb_chunks/MixinClaimedChunkManager.java
@@ -1,40 +1,24 @@
 package org.valkyrienskies.mod.mixin.mod_compat.ftb_chunks;
 
-import dev.ftb.mods.ftbchunks.data.ClaimedChunk;
 import dev.ftb.mods.ftbchunks.data.ClaimedChunkManager;
-import dev.ftb.mods.ftblibrary.math.ChunkDimPos;
-import java.util.Map;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3d;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 
 @Pseudo
 @Mixin(ClaimedChunkManager.class)
 public abstract class MixinClaimedChunkManager {
     @Unique
     private Entity entity = null;
-    @Unique
-    private BlockPos pos = null;
-
-    @Shadow
-    public abstract @Nullable ClaimedChunk getChunk(ChunkDimPos pos);
-
-    @Shadow
-    @Final
-    private Map<ChunkDimPos, ClaimedChunk> claimedChunks;
 
     @ModifyVariable(method = "protect", at = @At("HEAD"), name = "entity", remap = false)
     private Entity ValkyrienSkies$entity(final Entity entity) {
@@ -42,22 +26,24 @@ public abstract class MixinClaimedChunkManager {
         return entity;
     }
 
-    @ModifyVariable(method = "protect", at = @At("HEAD"), name = "pos", remap = false)
-    private BlockPos ValkyrienSkies$entity(final BlockPos instance) {
-        this.pos = instance;
-        return pos;
-    }
-
-    @Inject(method = "getChunk", at = @At("RETURN"), cancellable = true, remap = false)
-    public void ValkyrienSkies$getChunk(final ChunkDimPos dimPos, final CallbackInfoReturnable<ClaimedChunk> cir) {
-        if (entity != null) {
-            final Level level = entity.level;
-            final Ship ship = VSGameUtilsKt.getShipManagingPos(level, pos);
-            if (ship != null && pos.getY() < level.getMaxBuildHeight() && pos.getY() > level.getMinBuildHeight()) {
-                final Vector3d vec =
-                    ship.getShipToWorld().transformPosition(new Vector3d(pos.getX(), pos.getY(), pos.getZ()));
-                cir.setReturnValue(getChunk(new ChunkDimPos(level, new BlockPos(vec.x, vec.y, vec.z))));
-            }
+    @ModifyArg(
+        method = "protect",
+        at = @At(
+            value = "INVOKE",
+            target = "Ldev/ftb/mods/ftblibrary/math/ChunkDimPos;<init>(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"
+        )
+    )
+    private BlockPos ValkyrienSkies$newChunkDimPos(final BlockPos pos) {
+        if (entity == null) {
+            return pos;
         }
+
+        final Ship ship = VSGameUtilsKt.getShipManagingPos(entity.level, pos);
+        if (ship == null) {
+            return pos;
+        }
+
+        final Vector3d vec = ship.getShipToWorld().transformPosition(new Vector3d(pos.getX(), pos.getY(), pos.getZ()));
+        return new BlockPos(VectorConversionsMCKt.toMinecraft(vec));
     }
 }
