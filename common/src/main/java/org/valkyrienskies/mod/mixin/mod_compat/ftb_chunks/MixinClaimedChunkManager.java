4ed0dd48650ad89b1e2eba15a048fc652b0d279b
TechTastic <74630543+TechTastic@users.noreply.github.com>
2023-01-03 16:10:35 -0600

    [1.18.2] FTB Chunks Compatibility (#291)
    * Added MixinClaimedChunkManager to pass Shipyard positions to worldspace to be tested against
    Edited build.gradle:common to add FTB Chunks, Utilities, and Teams, as well as the CurseMaven repository
    Edited valkyrienskies-common.mixins.json to add MixinClaimedChunkManager
    
    * Edited MixinClaimedChunkManager to add a check for build height to allow ships to fly over claimed chunks
    
    * Edited MixinClaimedChunkManager to use the actual position
    
    * Fixed Crash
    
    Edited MixinClaimedChunkManager to change the position passed to the newly created ChunkDimPos instance for getChunk
    
    * Re-added build height check to allow ships to fly over claims without falling under claim's protection
    
    * Added a new Config Segment for FTB Chunks with 2 Config Options to disable ships being protected and to disable ships being protected above/below build height
    
    * remove maven repo
    
    Co-authored-by: Rubydesic <rubydesic@gmail.com>

---
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/ftb_chunks/MixinClaimedChunkManager.java
@@ -0,0 +1,62 @@
+package org.valkyrienskies.mod.mixin.mod_compat.ftb_chunks;
+
+import dev.ftb.mods.ftbchunks.data.ClaimedChunkManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.config.VSGameConfig;
+import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
+
+@Pseudo
+@Mixin(ClaimedChunkManager.class)
+public abstract class MixinClaimedChunkManager {
+    @Unique
+    private Entity entity = null;
+
+    @ModifyVariable(method = "protect", at = @At("HEAD"), name = "entity", remap = false)
+    private Entity ValkyrienSkies$entity(final Entity entity) {
+        this.entity = entity;
+        return entity;
+    }
+
+    @ModifyArg(
+        method = "protect",
+        at = @At(
+            value = "INVOKE",
+            target = "Ldev/ftb/mods/ftblibrary/math/ChunkDimPos;<init>(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"
+        )
+    )
+    private BlockPos ValkyrienSkies$newChunkDimPos(final BlockPos pos) {
+        if (entity == null && !VSGameConfig.SERVER.getFTBChunks().getShipsProtectedByClaims()) {
+            return pos;
+        }
+
+        final Level level = entity.level;
+
+        final Ship ship = VSGameUtilsKt.getShipManagingPos(level, pos);
+        if (ship == null) {
+            return pos;
+        }
+
+        final Vector3d vec = ship.getShipToWorld().transformPosition(new Vector3d(pos.getX(), pos.getY(), pos.getZ()));
+        final BlockPos newPos = new BlockPos(VectorConversionsMCKt.toMinecraft(vec));
+
+        if (
+            (newPos.getY() > level.getMaxBuildHeight() || newPos.getY() < level.getMinBuildHeight()) &&
+                !VSGameConfig.SERVER.getFTBChunks().getShipsProtectionOutOfBuildHeight()
+        ) {
+            return pos;
+        }
+
+        return newPos;
+    }
+}
