c83424165183ce0621473b88570718ca2d953337
StewStrong <stew.strong.420@gmail.com>
2023-01-16 04:45:00 -0700

    Fixed contraption wings on fabric

---
+++ fabric/src/main/kotlin/org/valkyrienskies/mod/fabric/common/CreateConversions.kt
@@ -0,0 +1,13 @@
+package org.valkyrienskies.mod.fabric.common
+
+import org.joml.Matrix3d
+import org.valkyrienskies.mod.fabric.mixin.compat.create.Matrix3dAccessor
+
+fun com.simibubi.create.foundation.collision.Matrix3d.toJOML(): Matrix3d {
+    val accessor = this as Matrix3dAccessor
+    return Matrix3d(
+        accessor.m00, accessor.m01, accessor.m02,
+        accessor.m10, accessor.m11, accessor.m12,
+        accessor.m20, accessor.m21, accessor.m22
+    ).transpose()
+}
