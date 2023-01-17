df47d4b7c05b2540dd489b133f74d03405a19dce
Rubydesic <rubydesic@gmail.com>
2023-01-17 11:13:29 -0500

    fix: flywheel issue

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/flywheel/MixinInstanceWorld.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/flywheel/MixinInstanceWorld.java
@@ -1,13 +1,15 @@
 package org.valkyrienskies.mod.mixin.mod_compat.flywheel;
 
+import com.jozufozu.flywheel.api.MaterialManager;
 import com.jozufozu.flywheel.backend.gl.GlStateTracker;
 import com.jozufozu.flywheel.backend.instancing.InstanceManager;
 import com.jozufozu.flywheel.backend.instancing.InstanceWorld;
+import com.jozufozu.flywheel.backend.instancing.ParallelTaskEngine;
+import com.jozufozu.flywheel.backend.instancing.batching.BatchingEngine;
+import com.jozufozu.flywheel.backend.instancing.instancing.InstancingEngine;
 import com.jozufozu.flywheel.event.RenderLayerEvent;
 import com.mojang.math.Matrix4f;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.joml.Matrix4d;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Final;
@@ -18,19 +20,23 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.ClientShip;
+import org.valkyrienskies.mod.common.VSClientGameUtils;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
+import org.valkyrienskies.mod.mixinducks.MixinBlockEntityInstanceManagerDuck;
 import org.valkyrienskies.mod.mixinducks.MixinInstancingEngineDuck;
-import org.valkyrienskies.mod.mixinducks.MixinTileInstanceManagerDuck;
 
 @Pseudo
 @Mixin(value = InstanceWorld.class, remap = false)
 public class MixinInstanceWorld {
-    @Unique
-    private static final Logger LOGGER = LogManager.getLogger("VS2 flywheel.client.MixinInstanceWorld");
 
     @Shadow
     @Final
     protected InstanceManager<BlockEntity> blockEntityInstanceManager;
+
+    @Shadow
+    @Final
+    public ParallelTaskEngine taskEngine;
 
     @Inject(
         method = "renderLayer",
@@ -42,10 +48,19 @@ public class MixinInstanceWorld {
     void renderShipTiles(final RenderLayerEvent event, final CallbackInfo ci) {
         //not sure if restoreState stuff should be here or in the ((MixinInstancingEngineDuck) manager).render() method
         final GlStateTracker.State restoreState = GlStateTracker.getRestoreState();
-        final var shipManagers = ((MixinTileInstanceManagerDuck) blockEntityInstanceManager).getShipInstancingEngines();
+        final var shipManagers =
+            ((MixinBlockEntityInstanceManagerDuck) blockEntityInstanceManager).getShipMaterialManagers();
 
         shipManagers.forEach((ship, manager) -> {
-            final Vector3d origin = VectorConversionsMCKt.toJOMLD(manager.getOriginCoordinate());
+            render(ship, manager, event);
+        });
+        restoreState.restore();
+    }
+
+    @Unique
+    void render(final ClientShip ship, final MaterialManager manager, final RenderLayerEvent event) {
+        if (manager instanceof InstancingEngine<?> engine) {
+            final Vector3d origin = VectorConversionsMCKt.toJOMLD(engine.getOriginCoordinate());
 
             final Matrix4d viewProjection = VectorConversionsMCKt.toJOML(event.viewProjection);
 
@@ -61,15 +76,20 @@ public class MixinInstanceWorld {
 
             final Matrix4f fnlProj = VectorConversionsMCKt.toMinecraft(finalProjection);
 
-            ((MixinInstancingEngineDuck) manager).render(
+            ((MixinInstancingEngineDuck) engine).render(
                 fnlProj,
                 camInShipLocal.x,
                 camInShipLocal.y,
                 camInShipLocal.z,
                 event.layer
             );
-        });
-        restoreState.restore();
-
+        } else if (manager instanceof BatchingEngine engine) {
+            event.stack.pushPose();
+            VSClientGameUtils.multiplyWithShipToWorld(event.stack, ship);
+            engine.render(taskEngine, event);
+            event.stack.popPose();
+        } else {
+            throw new IllegalArgumentException("unrecognized engine");
+        }
     }
 }
