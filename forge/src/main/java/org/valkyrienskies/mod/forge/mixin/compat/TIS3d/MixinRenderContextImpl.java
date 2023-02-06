93fef05435518ae9b8782cf760ae423d21df4e3c
walksanatora <walkerffo22@gmail.com>
2023-02-05 18:46:01 -0800

    mc forge says this is better for compat

--- forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/TIS3d/MixinRenderContextImpl.java
+++ forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/TIS3d/MixinRenderContextImpl.java
@@ -4,11 +4,15 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import li.cil.tis3d.client.renderer.RenderContextImpl;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Position;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
+import org.valkyrienskies.core.api.ships.ClientShip;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 @Pseudo
@@ -22,26 +26,17 @@ public abstract class MixinRenderContextImpl {
     )
     private boolean ValkyrienSkies$closerToCenterThan(final BlockPos instance, final Position pos, final float dist,
         final Operation<Boolean> orig) {
-        return VSGameUtilsKt.squaredDistanceBetweenInclShips(
-            Minecraft.getInstance().level,
-            instance.getX(),
-            instance.getY(),
-            instance.getZ(),
-            pos.x(),
-            pos.y(),
-            pos.z()
-        ) <= dist;
-        // I initially did this, but then ruby pointed out the function above
-        //final ClientLevel lvl = Minecraft.getInstance().level;
-        //final ClientShip ship = VSGameUtilsKt.getShipObjectManagingPos(lvl, instance);
-        //VSGameUtilsKt.squaredDistanceBetweenInclShips()
-        //if (ship != null) {
-        //    final Vector3d spos = ship.getTransform().getWorldToShip().transformPosition(
-        //        new Vector3d(pos.x(), pos.y(), pos.z())
-        //    );
-        //    return orig.call(instance, new Vec3(spos.x, spos.y, spos.z), dist);
-        //} else {
-        //    return orig.call(instance, pos, dist);
-        //}
+        // this code has been deemed better by those at the forge discord (since it calls the origional function)
+        final ClientLevel lvl = Minecraft.getInstance().level;
+        final ClientShip ship = VSGameUtilsKt.getShipObjectManagingPos(lvl, instance);
+        VSGameUtilsKt.squaredDistanceBetweenInclShips()
+        if (ship != null) {
+            final Vector3d spos = ship.getTransform().getWorldToShip().transformPosition(
+                new Vector3d(pos.x(), pos.y(), pos.z())
+            );
+            return orig.call(instance, new Vec3(spos.x, spos.y, spos.z), dist);
+        } else {
+            return orig.call(instance, pos, dist);
+        }
     }
 }
