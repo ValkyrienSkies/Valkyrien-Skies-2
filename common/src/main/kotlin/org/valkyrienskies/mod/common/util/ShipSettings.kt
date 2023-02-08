1d5acb83d8539ce4603bf0935069b4846899c26c
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-02-08 18:29:04 +0100

    ShipSettings (#413)

---
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/util/ShipSettings.kt
@@ -0,0 +1,19 @@
+package org.valkyrienskies.mod.common.util
+
+import org.valkyrienskies.core.api.ships.ServerShip
+import org.valkyrienskies.core.api.ships.getAttachment
+import org.valkyrienskies.core.api.ships.saveAttachment
+
+/**
+ * A attachment that stores ship specific settings.
+ */
+data class ShipSettings(
+    /**
+     * Should the ship chunks try to generate? If true it will generate chunks in the shipyard.
+     * You normally don't want this EVER
+     */
+    val shouldGenerateChunks: Boolean = false
+)
+
+val ServerShip.settings: ShipSettings
+    get() = getAttachment<ShipSettings>() ?: ShipSettings().also { saveAttachment(it) }
