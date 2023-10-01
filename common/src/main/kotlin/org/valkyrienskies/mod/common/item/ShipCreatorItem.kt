57f9cc246813b30cb2897d671bee194969c3a3f4
StewStrong <stew.strong.420@gmail.com>
2023-10-01 14:55:01 -0600

    Fixed ship creator item not handling scale correctly

--- common/src/main/kotlin/org/valkyrienskies/mod/common/item/ShipCreatorItem.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/item/ShipCreatorItem.kt
@@ -60,7 +60,7 @@ class ShipCreatorItem(
                     val newShipPosInShipyard = blockPos.toJOMLD().add(0.5, 0.5, 0.5)
                     val newShipRotation = parentShip.transform.shipToWorldRotation
                     var newShipScaling = parentShip.transform.shipToWorldScaling.mul(scale, Vector3d())
-                    if (newShipScaling.x() < scale) {
+                    if (newShipScaling.x() < minScaling) {
                         // Do not allow scaling to go below minScaling
                         newShipScaling = Vector3d(minScaling, minScaling, minScaling)
                     }
