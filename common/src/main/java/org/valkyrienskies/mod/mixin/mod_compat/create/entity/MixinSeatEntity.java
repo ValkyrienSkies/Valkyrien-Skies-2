62d1c0e974fdf54f8eeacee87c60ab370bc4daab
StewStrong <stewstronger@gmail.com>
2023-11-19 21:32:57 -0700

    Fixed dismounting destroyed seats teleporting entities into the sky

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/create/entity/MixinSeatEntity.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/create/entity/MixinSeatEntity.java
@@ -3,11 +3,13 @@ package org.valkyrienskies.mod.mixin.mod_compat.create.entity;
 import com.simibubi.create.content.contraptions.actors.seat.SeatEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
@@ -17,28 +19,45 @@ import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 public abstract class MixinSeatEntity extends Entity {
 
     @Shadow
-    public static double getCustomEntitySeatOffset(Entity entity) {
+    public static double getCustomEntitySeatOffset(final Entity entity) {
         return 0;
     }
 
-    public MixinSeatEntity(EntityType<?> entityType, Level level) {
+    public MixinSeatEntity(final EntityType<?> entityType, final Level level) {
         super(entityType, level);
     }
 
-    @Override
-    public void positionRider(@NotNull Entity passenger) {
+    /**
+     * @author Triode
+     * @reason Fix rider position
+     */
+    @Overwrite
+    public void positionRider(@NotNull final Entity passenger, final Entity.MoveFunction moveFunction) {
         if (!this.hasPassenger(passenger))
             return;
 
-        double d0 = this.getY() + this.getPassengersRidingOffset() + passenger.getMyRidingOffset();
+        final double d0 = this.getY() + this.getPassengersRidingOffset() + passenger.getMyRidingOffset();
         Vec3 riderPos = new Vec3(this.getX(), d0 + getCustomEntitySeatOffset(passenger), this.getZ());
 
-        Ship ship = VSGameUtilsKt.getShipManagingPos(passenger.level, riderPos.x, riderPos.y, riderPos.z);
+        final Ship ship = VSGameUtilsKt.getShipManagingPos(passenger.level, riderPos.x, riderPos.y, riderPos.z);
         if (VSGameUtilsKt.isBlockInShipyard(passenger.level, riderPos.x, riderPos.y, riderPos.z) && ship != null) {
-            Vector3d tempVec = VectorConversionsMCKt.toJOML(riderPos);
+            final Vector3d tempVec = VectorConversionsMCKt.toJOML(riderPos);
             ship.getShipToWorld().transformPosition(tempVec, tempVec);
             riderPos = VectorConversionsMCKt.toMinecraft(tempVec);
         }
         passenger.setPos(riderPos);
+    }
+
+    /**
+     * @author Triode
+     * @reason Fix dismount position when ship or seat is destroyed
+     */
+    @Overwrite
+    public @NotNull Vec3 getDismountLocationForPassenger(final @NotNull LivingEntity livingEntity) {
+        if (VSGameUtilsKt.isBlockInShipyard(level, position()) && VSGameUtilsKt.getShipManagingPos(level, position()) == null) {
+            // Don't teleport to the ship if we can't find the ship
+            return livingEntity.position();
+        }
+        return super.getDismountLocationForPassenger(livingEntity).add(0, 0.5f, 0);
     }
 }
--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/create/pr/MixinSeatBlock.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/create/pr/MixinSeatBlock.java
@@ -1,24 +1,22 @@
 package org.valkyrienskies.mod.mixin.mod_compat.create.pr;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.contraptions.actors.seat.SeatBlock;
 import com.simibubi.create.content.contraptions.actors.seat.SeatEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3d;
 import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.util.EntityDraggingInformation;
 import org.valkyrienskies.mod.common.util.IEntityDraggingInformationProvider;
 
 @Mixin(SeatBlock.class)
@@ -33,58 +31,46 @@ public abstract class MixinSeatBlock extends Block {
      */
     @Overwrite(remap = false)
     public static void sitDown(final Level world, final BlockPos pos, final Entity entity) {
-        if (world.isClientSide)
+        if (world.isClientSide) {
             return;
+        }
         final SeatEntity seat = new SeatEntity(world, pos);
         seat.setPos(pos.getX() + .5, pos.getY(), pos.getZ() + .5);
         world.addFreshEntity(seat);
         entity.startRiding(seat, true);
-        if (entity instanceof final TamableAnimal ta)
+        if (entity instanceof final TamableAnimal ta) {
             ta.setInSittingPose(true);
-    }
-
-    @Shadow
-    public static boolean canBePickedUp(final Entity passenger) {
-        throw new IllegalStateException();
-    }
-
-    @Shadow
-    public static boolean isSeatOccupied(final Level world, final BlockPos pos) {
-        throw new IllegalStateException();
+        }
     }
 
     /**
      * @author Triode
      * @reason Fix entities not sitting in seats on ships
      */
-    @Overwrite
-    public void updateEntityAfterFallOn(final @NotNull BlockGetter reader, final Entity entity) {
-        BlockPos pos = entity.blockPosition();
-        final Long shipStandingOnId = ((IEntityDraggingInformationProvider) entity).getDraggingInformation().getLastShipStoodOn();
-        if (shipStandingOnId != null) {
-            final Ship ship = VSGameUtilsKt.getShipObjectWorld(entity.level).getLoadedShips().getById(shipStandingOnId);
-            if (ship != null) {
-                final Vector3dc posInShip = ship.getTransform().getWorldToShip().transformPosition(entity.getX(), entity.getY(), entity.getZ(), new Vector3d());
-                pos = new BlockPos(posInShip.x(), posInShip.y(), posInShip.z());
+    @WrapOperation(
+        method = "updateEntityAfterFallOn",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/Entity;blockPosition()Lnet/minecraft/core/BlockPos;"
+        ),
+        remap = false
+    )
+    private BlockPos wrapBlockPosInUpdateEntityAfterFallOn(
+        final Entity entity,
+        final Operation<BlockPos> getBlockPosition
+    ) {
+        final EntityDraggingInformation draggingInformation = ((IEntityDraggingInformationProvider) entity).getDraggingInformation();
+        if (draggingInformation.isEntityBeingDraggedByAShip()) {
+            final Long shipStandingOnId = draggingInformation.getLastShipStoodOn();
+            if (shipStandingOnId != null) {
+                final Ship ship = VSGameUtilsKt.getShipObjectWorld(entity.level).getLoadedShips().getById(shipStandingOnId);
+                if (ship != null) {
+                    final Vector3dc posInShip = ship.getTransform().getWorldToShip()
+                        .transformPosition(entity.getX(), entity.getY(), entity.getZ(), new Vector3d());
+                    return new BlockPos(posInShip.x(), posInShip.y(), posInShip.z());
+                }
             }
         }
-        if (entity instanceof Player || !(entity instanceof LivingEntity) || !canBePickedUp(entity) || isSeatOccupied(entity.level, pos)) {
-            if (entity.isSuppressingBounce()) {
-                super.updateEntityAfterFallOn(reader, entity);
-                return;
-            }
-
-            final Vec3 vec3 = entity.getDeltaMovement();
-            if (vec3.y < 0.0D) {
-                final double d0 = entity instanceof LivingEntity ? 1.0D : 0.8D;
-                entity.setDeltaMovement(vec3.x, -vec3.y * (double) 0.66F * d0, vec3.z);
-            }
-
-            return;
-        }
-        if (reader.getBlockState(pos).getBlock() != this) {
-            return;
-        }
-        sitDown(entity.level, pos, entity);
+        return getBlockPosition.call(entity);
     }
 }
