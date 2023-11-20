57ff5e9c2adbf6e671c91ad9fe193084876256a8
StewStrong <stewstronger@gmail.com>
2023-11-20 01:51:08 -0700

    Fixed forge server not launching

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/create/entity/MixinSeatEntity.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/create/entity/MixinSeatEntity.java
@@ -7,45 +7,14 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
-import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
-import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 
 @Mixin(SeatEntity.class)
 public abstract class MixinSeatEntity extends Entity {
-
-    @Shadow
-    public static double getCustomEntitySeatOffset(final Entity entity) {
-        return 0;
-    }
-
     public MixinSeatEntity(final EntityType<?> entityType, final Level level) {
         super(entityType, level);
-    }
-
-    /**
-     * @author Triode
-     * @reason Fix rider position
-     */
-    @Overwrite
-    public void positionRider(@NotNull final Entity passenger, final Entity.MoveFunction moveFunction) {
-        if (!this.hasPassenger(passenger))
-            return;
-
-        final double d0 = this.getY() + this.getPassengersRidingOffset() + passenger.getMyRidingOffset();
-        Vec3 riderPos = new Vec3(this.getX(), d0 + getCustomEntitySeatOffset(passenger), this.getZ());
-
-        final Ship ship = VSGameUtilsKt.getShipManagingPos(passenger.level, riderPos.x, riderPos.y, riderPos.z);
-        if (VSGameUtilsKt.isBlockInShipyard(passenger.level, riderPos.x, riderPos.y, riderPos.z) && ship != null) {
-            final Vector3d tempVec = VectorConversionsMCKt.toJOML(riderPos);
-            ship.getShipToWorld().transformPosition(tempVec, tempVec);
-            riderPos = VectorConversionsMCKt.toMinecraft(tempVec);
-        }
-        passenger.setPos(riderPos);
     }
 
     /**
