15e9b4ddb0ad9cefbaaa609d57baff5b77d2faa1
illucc <illucizer@gmail.com>
2023-12-31 02:23:08 +0200

    teeny tiny fixes

--- forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/mekanism/MixinRadiationManager.java
+++ forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/mekanism/MixinRadiationManager.java
@@ -1,5 +1,6 @@
 package org.valkyrienskies.mod.forge.mixin.compat.mekanism;
 
+import java.util.Objects;
 import mekanism.api.Coord4D;
 import mekanism.common.lib.radiation.RadiationManager;
 import net.minecraft.core.BlockPos;
@@ -18,7 +19,7 @@ public class MixinRadiationManager  {
     @ModifyVariable(remap = false, ordinal = 0, method = "radiate(Lmekanism/api/Coord4D;D)V", at = @At("HEAD"), argsOnly = true)
     private Coord4D MixinDumpRadiation(final Coord4D coord4D) {
         final ResourceKey<Level> resourceKey = coord4D.dimension;
-        final Level level = ValkyrienSkiesMod.getCurrentServer().getLevel(resourceKey);
+        final Level level = Objects.requireNonNull(ValkyrienSkiesMod.getCurrentServer()).getLevel(resourceKey);
         final Ship ship = VSGameUtilsKt.getShipManagingPos(level, coord4D.getPos());
         if (ship == null){
             return coord4D;
