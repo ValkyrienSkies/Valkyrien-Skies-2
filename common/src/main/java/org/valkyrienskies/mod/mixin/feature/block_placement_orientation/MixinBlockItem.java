5e053f1d934a5bc664dd22ef2838723a3066467b
ewoudje <7384674+ewoudje@users.noreply.github.com>
2024-01-30 16:26:13 +0100

    If an error happens on place, untransform

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/block_placement_orientation/MixinBlockItem.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/block_placement_orientation/MixinBlockItem.java
@@ -1,50 +1,34 @@
 package org.valkyrienskies.mod.mixin.feature.block_placement_orientation;
 
-import net.minecraft.world.InteractionResult;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.valkyrienskies.mod.common.PlayerUtil;
 
 @Mixin(BlockItem.class)
 public abstract class MixinBlockItem {
-    
-    @Inject(
+
+    @WrapOperation(
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/item/BlockItem;getPlacementState(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/level/block/state/BlockState;"
         ),
-        method = "place",
-        locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private void transformPlayerWhenPlacing(final BlockPlaceContext ignore,
-        final CallbackInfoReturnable<InteractionResult> cir, final BlockPlaceContext context) {
-        if (context == null || context.getPlayer() == null) {
-            return;
-        }
-
-        PlayerUtil.transformPlayerTemporarily(context.getPlayer(), context.getLevel(), context.getClickedPos());
-    }
-
-    @Inject(
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/item/BlockItem;getPlacementState(Lnet/minecraft/world/item/context/BlockPlaceContext;)Lnet/minecraft/world/level/block/state/BlockState;",
-            shift = Shift.AFTER
-        ),
         method = "place"
     )
-    private void untransformPlayerAfterPlacing(final BlockPlaceContext context,
-        final CallbackInfoReturnable<InteractionResult> cir) {
-        if (context.getPlayer() == null) {
-            return;
+    private BlockState transformPlayerWhenPlacing(BlockItem _instance, BlockPlaceContext _ctx, Operation<BlockState> original, final BlockPlaceContext ctx) {
+        if (ctx == null || ctx.getPlayer() == null) {
+            return null;
         }
 
-        PlayerUtil.untransformPlayer(context.getPlayer());
+        return PlayerUtil.transformPlayerTemporarily(
+            ctx.getPlayer(),
+            ctx.getLevel(),
+            ctx.getClickedPos(),
+            original::call
+        );
     }
 }
