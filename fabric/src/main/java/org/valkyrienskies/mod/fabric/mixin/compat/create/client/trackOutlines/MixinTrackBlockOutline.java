d9e0db7d3354dcd03c4a8a40c3d72ed3ee664cba
MrSterner <32911664+mrsterner@users.noreply.github.com>
2024-03-30 17:55:59 +0100

    Fixed patch f crash and added some support for Create utilities (#743)
    * Added support for Create utilities VoidValueBox position and fixed MixinTrackBlockOutline
    
    * added javadoc i guess

---
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/create/client/trackOutlines/MixinTrackBlockOutline.java
@@ -0,0 +1,134 @@
+package org.valkyrienskies.mod.fabric.mixin.compat.create.client.trackOutlines;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.simibubi.create.content.trains.track.TrackBlockOutline;
+import com.simibubi.create.content.trains.track.TrackBlockOutline.BezierPointSelection;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Quaterniond;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.core.impl.game.ships.ShipObjectClient;
+import org.valkyrienskies.mod.common.VSClientGameUtils;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
+
+@Mixin(TrackBlockOutline.class)
+public class MixinTrackBlockOutline {
+    @Unique
+    private static Vec3 valkyrienskies$cameraVec3;
+    @Unique
+    private static Vec3 valkyrienskies$vec;
+    @Unique
+    private static Vec3 valkyrienskies$angles;
+
+    @Inject(method = "drawCurveSelection",
+        at = @At(value = "INVOKE",
+            target = "Lcom/simibubi/create/content/trains/track/TrackBlockOutline$BezierPointSelection;angles()Lnet/minecraft/world/phys/Vec3;"),
+        locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void harvestDrawCurveSelection(final PoseStack ms, final MultiBufferSource buffer, final Vec3 camera,
+        final CallbackInfo ci, final Minecraft mc,
+        final BezierPointSelection result, final VertexConsumer vb, final Vec3 vec) {
+        valkyrienskies$cameraVec3 = camera;
+        valkyrienskies$vec = result.vec();
+        valkyrienskies$angles = result.angles();
+    }
+    @ModifyArg(method = "drawCurveSelection",
+        at = @At(value = "INVOKE",
+            target = "Lcom/simibubi/create/content/trains/track/TrackBlockOutline;renderShape(Lnet/minecraft/world/phys/shapes/VoxelShape;Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;Ljava/lang/Boolean;)V"),
+        index = 1)
+    private static PoseStack redirectTransformStackTranslate(final PoseStack ms) {
+
+        final Level level = Minecraft.getInstance().level;
+        if (level != null && valkyrienskies$vec != null) {
+            final ShipObjectClient ship;
+            if ((ship = (ShipObjectClient) VSGameUtilsKt.getShipManagingPos(level, valkyrienskies$vec)) != null) {
+                final Quaterniond rotation = new Quaterniond().identity();
+                final Quaterniond yawQuat = new Quaterniond().rotateY(valkyrienskies$angles.y);
+                final Quaterniond pitchQuat = new Quaterniond().rotateX(valkyrienskies$angles.x);
+
+                yawQuat.mul(pitchQuat, rotation);
+                ship.getRenderTransform().getShipToWorldRotation().mul(rotation, rotation);
+
+                final Vector3d worldVec = ship.getRenderTransform().getShipToWorld()
+                    .transformPosition(
+                        new Vector3d(valkyrienskies$vec.x, valkyrienskies$vec.y + .125, valkyrienskies$vec.z),
+                        new Vector3d());
+
+                ms.popPose();
+                ms.pushPose();
+                ms.translate(worldVec.x - valkyrienskies$cameraVec3.x,
+                    worldVec.y - valkyrienskies$cameraVec3.y,
+                    worldVec.z - valkyrienskies$cameraVec3.z);
+                ms.mulPose(VectorConversionsMCKt.toFloat(rotation));
+                ms.translate(-.5, -.125f, -.5);
+            }
+        }
+        return ms;
+    }
+
+    @Unique
+    private static Camera valkyrienskies$info;
+    @Unique
+    private static BlockHitResult valkyrienskies$hitResult;
+
+    @ModifyArg(method = "drawCustomBlockSelection", at = @At(value = "INVOKE",
+        target = "Lnet/minecraft/world/level/border/WorldBorder;isWithinBounds(Lnet/minecraft/core/BlockPos;)Z"))
+    private static BlockPos modIsWithinBounds(final BlockPos blockPos) {
+        final Level level = Minecraft.getInstance().level;
+        if (level != null) {
+            final Ship ship;
+            if ((ship = VSGameUtilsKt.getShipManagingPos(level, blockPos)) != null) {
+                return BlockPos.containing(VectorConversionsMCKt.toMinecraft(ship.getShipToWorld()
+                    .transformPosition(VectorConversionsMCKt.toJOMLD(blockPos))));
+            }
+        }
+        return blockPos;
+    }
+
+    @Inject(method = "drawCustomBlockSelection",
+        at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V"))
+    private static void harvest(final LevelRenderer context, final Camera info, final HitResult hitResult,
+        final float partialTicks,
+        final PoseStack ms, final MultiBufferSource buffers, final CallbackInfoReturnable<Boolean> cir) {
+        valkyrienskies$info = info;
+        valkyrienskies$hitResult = (BlockHitResult) hitResult;
+    }
+
+    @Redirect(method = "drawCustomBlockSelection",
+        at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V"))
+    private static void redirectTranslate(final PoseStack instance, final double d, final double e, final double f) {
+        final Level level = Minecraft.getInstance().level;
+        if (level != null) {
+            final ShipObjectClient ship;
+            if ((ship = (ShipObjectClient) VSGameUtilsKt.getShipManagingPos(level,
+                valkyrienskies$hitResult.getBlockPos())) != null) {
+                final Vec3 camPos = valkyrienskies$info.getPosition();
+                VSClientGameUtils.transformRenderWithShip(ship.getRenderTransform(), instance,
+                    valkyrienskies$hitResult.getBlockPos(),
+                    camPos.x, camPos.y, camPos.z);
+            } else {
+                instance.translate(d, e, f);
+            }
+        } else {
+            instance.translate(d, e, f);
+        }
+    }
+}
