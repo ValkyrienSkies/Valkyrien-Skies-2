e21b52807b255305c65030e263a7e083eed4bdd8
Potato <77798144+ThePlasticPotato@users.noreply.github.com>
2024-06-30 12:29:59 -0400

    fix: nvm, i cant read hash numbers

--- common/src/main/kotlin/org/valkyrienskies/mod/api/VsApi.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/api/VsApi.kt
@@ -17,7 +17,7 @@ import org.valkyrienskies.mod.api.events.PreRenderShipEvent
 import org.valkyrienskies.mod.api.events.RegisterBlockStateEvent
 
 @NonExtendable
-interface VsApi : VsCoreApi {
+interface VsApi : VSCoreApi {
 
     /**
      * This event gets called when it's time to register physics block states for Minecraft block states.
