0d585eda6994d5bdd4fa75c52c4586a9897cc49d
m-doescode <80221594+m-doescode@users.noreply.github.co>
2024-08-05 18:30:09 +0200

    Added basic implementation of teleporting disconnected players back onto ships

---
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/teleport_reconnected_player_to_ship/MixinServerPlayer.java
@@ -0,0 +1,89 @@
+package org.valkyrienskies.mod.mixin.feature.teleport_reconnected_player_to_ship;
+
+import com.mojang.authlib.GameProfile;
+import javax.annotation.Nullable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.util.EntityDraggingInformation;
+import org.valkyrienskies.mod.common.util.IEntityDraggingInformationProvider;
+
+@Mixin(ServerPlayer.class)
+public abstract class MixinServerPlayer extends Player {
+
+    @Shadow
+    public abstract ServerLevel serverLevel();
+
+    public MixinServerPlayer(final Level level, final BlockPos blockPos, final float f,
+        final GameProfile gameProfile) {
+        super(level, blockPos, f, gameProfile);
+        throw new IllegalStateException("Unreachable");
+    }
+
+    @Inject(method = "readAdditionalSaveData", at = @At("RETURN"))
+    void teleportToShip(final CompoundTag compoundTag, final CallbackInfo ci) {
+        if (!compoundTag.contains("LastShipId"))
+            return; // Player did not disconnect off of any ship
+
+        final long lastShipId = compoundTag.getLong("LastShipId");
+        System.out.println("Last ship id is: " + lastShipId);
+
+        final Ship ship = VSGameUtilsKt.getShipObjectWorld(serverLevel()).getAllShips().getById(lastShipId);
+        System.out.println("Should teleport to: " + ship);
+
+        // If ship doesn't exist, don't bother
+        if (ship == null)
+            return;
+
+        // Translate ship coords to world coords
+        final double x = compoundTag.getDouble("RelativeShipX");
+        final double y = compoundTag.getDouble("RelativeShipY");
+        final double z = compoundTag.getDouble("RelativeShipZ");
+
+        final Vector3d playerShipPosition = new Vector3d(x, y, z);
+        final Vector3d playerWorldPosition = ship.getShipToWorld().transformPosition(playerShipPosition);
+
+        setPos(playerWorldPosition.x, playerWorldPosition.y, playerWorldPosition.z);
+    }
+
+    @Inject(method = "addAdditionalSaveData", at = @At("RETURN"))
+    void rememberLastShip(final CompoundTag compoundTag, final CallbackInfo ci) {
+        final EntityDraggingInformation draggingInformation = ((IEntityDraggingInformationProvider) this).getDraggingInformation();
+
+        if (draggingInformation.getTicksSinceStoodOnShip() > 4)
+            return;
+
+        @Nullable final Long lastShipId = draggingInformation.getLastShipStoodOn();
+
+        if (lastShipId == null)
+            return;
+
+        compoundTag.putLong("LastShipId", lastShipId);
+        System.out.println("Saved as: " + lastShipId);
+
+        final Ship ship = VSGameUtilsKt.getShipObjectWorld(serverLevel()).getAllShips().getById(lastShipId);
+
+        if (ship == null)
+            return;
+
+        // Get position relative to ship
+        // (Technically, this grabs the position in the shipyard, but it works well enough...)
+        final Vector3d playerWorldPosition = new Vector3d(getX(), getY(), getZ());
+        final Vector3d playerShipPosition = ship.getWorldToShip().transformPosition(playerWorldPosition);
+
+        compoundTag.putDouble("RelativeShipX", playerShipPosition.x);
+        compoundTag.putDouble("RelativeShipY", playerShipPosition.y);
+        compoundTag.putDouble("RelativeShipZ", playerShipPosition.z);
+    }
+}
