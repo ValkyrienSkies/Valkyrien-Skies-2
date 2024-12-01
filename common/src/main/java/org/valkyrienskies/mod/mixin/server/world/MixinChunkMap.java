7c3c63deddc866995e98aa4aa5d0d12c22392c56
James Green <jgreenop22@gmail.com>
2024-11-30 17:16:32 -0800

    create logic for Mobs to save their position relative to a ship when unloading, then teleport back to ship when loading back in

--- common/src/main/java/org/valkyrienskies/mod/mixin/server/world/MixinChunkMap.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/server/world/MixinChunkMap.java
@@ -9,17 +9,25 @@ import java.util.function.Supplier;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.storage.DimensionDataStorage;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.apigame.world.IPlayer;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.entity.ShipyardPosSavable;
 import org.valkyrienskies.mod.common.util.MinecraftPlayer;
+import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
+
 
 @Mixin(ChunkMap.class)
 public abstract class MixinChunkMap {
@@ -100,6 +108,65 @@ public abstract class MixinChunkMap {
         );
 
         cir.setReturnValue(new ArrayList<>(watchingPlayers));
+    }
+
+    /**
+     * Save mob's shipyard position when it gets unloaded
+     *
+     * @author G_Mungus
+     */
+
+    @Inject(method = "removeEntity", at = @At("HEAD"))
+    protected void unloadEntityMixin(Entity entity, CallbackInfo info) {
+        if (entity instanceof Mob mob) {
+            Vector3d shipyardPos = valkyrienskies$getPosInShipyard(mob);
+            if (shipyardPos != null &&
+                VSGameUtilsKt.getShipManagingPos(this.level, shipyardPos) != null &&
+                ((ShipyardPosSavable)mob).valkyrienskies$getUnloadedShipyardPos() == null) {
+
+                ((ShipyardPosSavable)mob).valkyrienskies$setUnloadedShipyardPos(shipyardPos);
+            }
+        }
+    }
+
+    /**
+     * Teleport mob to correct position on ship when loaded back in
+     *
+     * @author G_Mungus
+     */
+
+    @Inject(method = "addEntity", at = @At("RETURN"))
+    protected void loadEntityMixin(Entity entity, CallbackInfo info) {
+        if (entity instanceof Mob mob) {
+            Vector3d shipyardPos = ((ShipyardPosSavable)mob).valkyrienskies$getUnloadedShipyardPos();
+            if(shipyardPos != null) {
+                mob.teleportTo(shipyardPos.x,
+                    shipyardPos.y,
+                    shipyardPos.z);
+
+                ((ShipyardPosSavable) mob).valkyrienskies$setUnloadedShipyardPos(null);
+            }
+        }
+    }
+
+
+    /**
+     * Helper method to get shipyard pos of mob on a ship
+     * <p>
+     * Implementation might look wierd but the way I originally had it didn't work while in the middle of chunk unloading
+     *
+     * @author G_Mungus
+     */
+    @Unique
+    private Vector3d valkyrienskies$getPosInShipyard(final Entity entity) {
+        List<Vector3d> vectors = VSGameUtilsKt.transformToNearbyShipsAndWorld(entity.level(), entity.getX(), entity.getY(), entity.getZ(), 1.0);
+
+        for (Vector3d vec : vectors) {
+            if (VSGameUtilsKt.isBlockInShipyard(entity.level(), VectorConversionsMCKt.toMinecraft(vec))) {
+                return vec;
+            }
+        }
+        return null;
     }
 
 }
