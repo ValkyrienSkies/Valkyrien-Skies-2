d7a5d836ede4a458ff7a0a13ae852ec6ecf5ed66
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-01-19 22:42:54 -0600

    Fixed both Turtles ability to leave Ships and re-implemented Turtles ability to collide with Ships and pass through their empty spaces

--- fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/cc_restitched/MixinTurtleMoveCommand.java
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/cc_restitched/MixinTurtleMoveCommand.java
@@ -4,9 +4,7 @@ import com.google.common.collect.Streams;
 import dan200.computercraft.api.turtle.TurtleCommandResult;
 import dan200.computercraft.shared.turtle.core.TurtleMoveCommand;
 import dan200.computercraft.shared.turtle.core.TurtlePlayer;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.ChunkPos;
@@ -15,7 +13,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -32,14 +29,17 @@ public abstract class MixinTurtleMoveCommand {
         if (cir.getReturnValue().isSuccess()) {
             final Ship ship = ValkyrienSkies.getShipManagingBlock(world, position);
             if (ship == null) {
-                final boolean notInAir = Streams
-                    .stream(ValkyrienSkies.positionToNearbyShipsAndWorld(world, position.getX() + 0.5, position.getY() + 0.5, position.getZ() + 0.5, 0.1))
-                    .map(pos -> ValkyrienSkies.getShipManagingBlock(world, pos))
-                    .map(s -> ValkyrienSkies.positionToShip(s, new Vector3d(position.getX() + 0.5, position.getY() + 0.5, position.getZ() + 0.5)))
-                    .map(pos -> world.getBlockState(new BlockPos(ValkyrienSkies.toMinecraft(pos))))
+                final List<Vector3d> nearbyShips = Streams.stream(ValkyrienSkies.positionToNearbyShips(world,
+                    position.getX() + 0.5, position.getY() + 0.5, position.getZ() + 0.5, 0.1)).toList();
+
+                final boolean inAir = nearbyShips.isEmpty() || nearbyShips
+                    .stream()
+                    .map(ValkyrienSkies::toMinecraft)
+                    .map(BlockPos::new)
+                    .map(world::getBlockState)
                     .anyMatch(BlockState::isAir);
 
-                if (notInAir) {
+                if (!inAir) {
                     cir.setReturnValue(TurtleCommandResult.failure("Movement obstructed by ship"));
                 }
             } else {
