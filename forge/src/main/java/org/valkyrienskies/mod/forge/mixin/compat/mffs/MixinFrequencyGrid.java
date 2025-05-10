86a944e8df79e57c55dda3a627bcfd7015f25214
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-05-10 18:45:41 -0500

    Updated to use `@Local(argsOnly = true) final Level level` at the behest of LlamaLad7

--- forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/mffs/MixinFrequencyGrid.java
+++ forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/mffs/MixinFrequencyGrid.java
@@ -2,6 +2,7 @@ package org.valkyrienskies.mod.forge.mixin.compat.mffs;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.su5ed.mffs.util.FrequencyGrid;
 import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.Level;
@@ -23,7 +24,7 @@ public abstract class MixinFrequencyGrid {
             target = "Lnet/minecraft/core/Vec3i;closerThan(Lnet/minecraft/core/Vec3i;D)Z"
         )
     )
-    private static boolean vs$closerThan(final Vec3i instance, final Vec3i vec3i, final double d, final Operation<Boolean> original, final Level level) {
+    private static boolean vs$closerThan(final Vec3i instance, final Vec3i vec3i, final double d, final Operation<Boolean> original, @Local(argsOnly = true) final Level level) {
         if (level == null) {
             return original.call(instance, vec3i, d);
         }
