27e24ade971c99f02bf79165e18db196eb868445
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-07-06 21:16:25 -0500

    IT WORKED

--- forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/oc2r/MixinProjectorDepthRenderer.java
+++ forge/src/main/java/org/valkyrienskies/mod/forge/mixin/compat/oc2r/MixinProjectorDepthRenderer.java
@@ -13,15 +13,14 @@ import org.valkyrienskies.mod.api.ValkyrienSkies;
 
 @Pseudo
 @Mixin(ProjectorDepthRenderer.class)
-public class MixinProjectorDepthRenderer {
-    @ModifyVariable(method = "renderProjectorDepths", at = @At(
-        value = "STORE",
-        target = "Lli/cil/oc2/client/renderer/ProjectorDepthRenderer;renderProjectorDepths(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/multiplayer/ClientLevel;FI)V"
-    ), remap = false)
-    private static Vec3 valkyrienskies$renderProjectorDepthsOnShips(Vec3 instance, @Local(argsOnly = true) ClientLevel level) {
-        Ship ship = ValkyrienSkies.getShipManagingBlock(level, instance);
-        if (ship == null) return instance;
-        
-        return ValkyrienSkies.positionToWorld(ship, instance);
+public abstract class MixinProjectorDepthRenderer {
+    @ModifyVariable(method = "renderProjectorDepths", at = @At("STORE"), ordinal = 1, remap = false)
+    private static Vec3 valkyrienskies$transformProjectorPosToWorld(Vec3 original, @Local(argsOnly = true) ClientLevel level) {
+        Ship ship = ValkyrienSkies.getShipManagingBlock(level, original);
+        if (ship == null) {
+            return original;
+        }
+
+        return ValkyrienSkies.positionToWorld(ship, original);
     }
 }
