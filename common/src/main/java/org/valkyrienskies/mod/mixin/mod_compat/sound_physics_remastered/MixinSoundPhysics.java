eb1ef2528d818945654fb0b729fe32b78f0c8c21
jcm236 <johnchristi236@gmail.com>
2025-07-23 21:05:28 +0530

    basic sound physics remastered compat

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/sound_physics_remastered/MixinSoundPhysics.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/sound_physics_remastered/MixinSoundPhysics.java
@@ -2,42 +2,47 @@ package org.valkyrienskies.mod.mixin.mod_compat.sound_physics_remastered;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.api.ships.ClientShip;
-import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.api.ValkyrienSkies;
 
 @Pseudo
-@Mixin(targets = "com.sonicether.soundphysics.SoundPhysics", remap = false)
+@Mixin(targets = "com.sonicether.soundphysics.SoundPhysics",remap = false)
 public abstract class MixinSoundPhysics {
 
-    @Shadow
-    public static void onPlaySound(final double posX, final double posY, final double posZ, final int sourceID) {
-        throw new AssertionError();
-    }
 
     @Inject(
         at = @At("HEAD"),
-        method = "onPlaySound",
+        method = "processSound(IDDDLnet/minecraft/sounds/SoundSource;Ljava/lang/String;Z)Lnet/minecraft/world/phys/Vec3;",
         cancellable = true
     )
-    private static void beforeOnPlaySound(
-        final double posX, final double posY, final double posZ, final int sourceID, final CallbackInfo ci) {
+    private static void beforeProcessSound(int source, double posX, double posY, double posZ, SoundSource category,
+        String sound, boolean auxOnly, CallbackInfoReturnable<Vec3> cir){
+
         final ClientLevel level = Minecraft.getInstance().level;
         if (level == null) {
             return;
         }
 
-        if (VSGameUtilsKt.getShipManagingPos(level, posX, posY, posZ) instanceof final ClientShip ship) {
+        if (ValkyrienSkies.getShipManagingBlock(level, posX, posY, posZ) instanceof final ClientShip ship) {
             final Vector3d inWorldPos = ship.getShipToWorld().transformPosition(new Vector3d(posX, posY, posZ));
-            onPlaySound(inWorldPos.x, inWorldPos.y, inWorldPos.z, sourceID);
-            ci.cancel();
+            cir.setReturnValue(processSound(source, inWorldPos.x, inWorldPos.y, inWorldPos.z, category, sound, auxOnly));
+            cir.cancel();
         }
+    }
+
+
+    @Shadow
+    public static Vec3 processSound(int source, double posX, double posY, double posZ, SoundSource category, String sound, boolean auxOnly) {
+        throw new AssertionError();
     }
 
 }
