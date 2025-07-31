d888f20a714c51002f7ef51fcf06a2c4acc0bb5c
zaafonin <zakharafoniam@gmail.com>
2025-07-31 15:31:25 +0300

    Fix for #1161 + fixed regressions introduced when Create got dual-versioned

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/common_create/MixinAirCurrent.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/common_create/MixinAirCurrent.java
@@ -1,8 +1,10 @@
 package org.valkyrienskies.mod.mixin.mod_compat.common_create;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.kinetics.fan.AirCurrent;
 import com.simibubi.create.content.kinetics.fan.IAirCurrentSource;
-import java.util.Iterator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
@@ -21,29 +23,21 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
 import org.valkyrienskies.mod.common.world.RaycastUtilsKt;
-import org.valkyrienskies.mod.compat.CreateCompat;
 import org.valkyrienskies.mod.mixinducks.mod_compat.create.IExtendedAirCurrentSource;
 
 @Mixin(AirCurrent.class)
 public abstract class MixinAirCurrent {
 
-    @Unique
-    private final float maxAcceleration = 5;
     @Shadow
     @Final
     public IAirCurrentSource source;
-    @Unique
-    private Vec3 transformedFlow = Vec3.ZERO;
-    @Unique
-    private float acceleration;
 
     @Unique
     private Ship getShip() {
@@ -55,8 +49,11 @@ public abstract class MixinAirCurrent {
             return null;
     }
 
-    @Inject(method = "getFlowLimit", at = @At("HEAD"), cancellable = true, remap = false)
-    private static void clipFlowLimit(Level level, BlockPos start, float max, Direction facing, CallbackInfoReturnable<Float> cir) {
+    @Inject(method = "getFlowLimit", at = @At("RETURN"), cancellable = true, remap = false)
+    private static void clipFlowLimit(Level level, BlockPos start, float originalMax, Direction facing, CallbackInfoReturnable<Float> cir) {
+        // First let Create do its job at finding block obstructions that will cap max length, then use this value as ship search range.
+        float max = cir.getReturnValue();
+
         Ship ship = VSGameUtilsKt.getShipManagingPos(level, start);
         if (ship != null) {
             Vector3d startVec = ship.getTransform().getShipToWorld().transformPosition(new Vector3d(start.getX() + 0.5, start.getY() + 0.5, start.getZ() + 0.5));
@@ -70,7 +67,12 @@ public abstract class MixinAirCurrent {
                             VectorConversionsMCKt.toMinecraft(startVec.add(direction.x, direction.y, direction.z)),
                             ClipContext.Block.OUTLINE,
                             ClipContext.Fluid.NONE,
-                            null));
+                            null), true,
+                            // Skipping own ship as block obstructions were already calculated by Create. Besides,
+                            // solid blocks like blaze burners or fan catalysts (from addons) can be used for processing
+                            // instead of obstructing airflow. Not accounting for this used to cause this bug:
+                            // https://github.com/ValkyrienSkies/Valkyrien-Skies-2/issues/1161
+                            ship.getId());
 
             // Distance from start to end but, its not squared so, slow -_-
             cir.setReturnValue((float) result.getLocation().distanceTo(mcStart));
@@ -94,7 +96,6 @@ public abstract class MixinAirCurrent {
         }
     }
 
-    // Require 0 because this mixin doesn't work in create 0.5.1f
     @Redirect(method = "tickAffectedEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/AABB;intersects(Lnet/minecraft/world/phys/AABB;)Z"), require = 0)
     private boolean redirectIntersects(AABB instance, AABB other) {
         Ship ship = getShip();
@@ -105,55 +106,41 @@ public abstract class MixinAirCurrent {
         } else return instance.intersects(other);
     }
 
-    // Require 0 because this mixin doesn't work in create 0.5.1f
-    // it doesn't exactly work in v6 either so this should probably get fixed
-    @Inject(
-        method = "tickAffectedEntities",
-        at = @At(
-                value = "INVOKE",
-                target = "Lnet/minecraft/world/entity/Entity;getDeltaMovement()Lnet/minecraft/world/phys/Vec3;"
-        ),
-        locals = LocalCapture.CAPTURE_FAILSOFT,
-        require = 0
+    // While getCenter is unstable between versions, it is only used as an argument of distanceTo which is only called once.
+    @ModifyArg(method = "tickAffectedEntities",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/phys/Vec3;distanceTo(Lnet/minecraft/world/phys/Vec3;)D", ordinal = 0),
+        index = 0
     )
-    private void harvester(Level world, CallbackInfo ci, Iterator iterator, Entity entity, Vec3i flow, float speed,
-        float sneakModifier, double entityDistance, double entityDistanceOld, float acceleration) {
+    private Vec3 transformCenter(Vec3 center) {
+        Ship ship = getShip();
+        if (ship != null && this.source.getAirCurrentWorld() != null) {
+            Vector3d tempVec = new Vector3d();
+            ship.getTransform().getShipToWorld().transformPosition(center.x, center.y, center.z, tempVec);
+            center = VectorConversionsMCKt.toMinecraft(tempVec);
+        }
+        return center;
+    }
+
+    // Ordinals used here are correct both for v0.5.1 and v6 and in fact are stable all the way from Create v0.3.
+    @WrapOperation(method = "tickAffectedEntities",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;setDeltaMovement(Lnet/minecraft/world/phys/Vec3;)V")
+    )
+    private void redirectSetDeltaMovement(Entity instance, Vec3 motion,
+        Operation<Void> original,
+        @Local(ordinal = 2) float acceleration, @Local(ordinal = 3) float maxAcceleration, @Local(ordinal = 0) Vec3i flow
+    ) {
         Ship ship = getShip();
         if (ship != null) {
             Vector3d tempVec = new Vector3d();
             ship.getTransform().getShipToWorld().transformDirection(flow.getX(), flow.getY(), flow.getZ(), tempVec);
-            transformedFlow = VectorConversionsMCKt.toMinecraft(tempVec);
-        }
-        this.acceleration = acceleration;
-    }
+            Vec3 transformedFlow = VectorConversionsMCKt.toMinecraft(tempVec);
 
-    // Require 0 because this mixin doesn't work in create 0.5.1f
-    @Redirect(method = "tickAffectedEntities",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;setDeltaMovement(Lnet/minecraft/world/phys/Vec3;)V"), require = 0
-    )
-    private void redirectSetDeltaMovement(Entity instance, Vec3 motion) {
-        Ship ship = getShip();
-        if (ship != null) {
             Vec3 previousMotion = instance.getDeltaMovement();
             double xIn = Mth.clamp(transformedFlow.x * acceleration - previousMotion.x, -maxAcceleration, maxAcceleration);
             double yIn = Mth.clamp(transformedFlow.y * acceleration - previousMotion.y, -maxAcceleration, maxAcceleration);
             double zIn = Mth.clamp(transformedFlow.z * acceleration - previousMotion.z, -maxAcceleration, maxAcceleration);
-            instance.setDeltaMovement(previousMotion.add(new Vec3(xIn, yIn, zIn).scale(1 / 8f)));
-        } else {
-            instance.setDeltaMovement(motion);
+            motion = previousMotion.add(new Vec3(xIn, yIn, zIn).scale(1 / 8f));
         }
-    }
-
-    // Require 0 because this mixin doesn't work in create 0.5.1f
-    @Redirect(method = "tickAffectedEntities", at = @At(value = "INVOKE", target = "Lnet/createmod/catnip/math/VecHelper;getCenterOf(Lnet/minecraft/core/Vec3i;)Lnet/minecraft/world/phys/Vec3;"), allow = 1, require = 0, remap = false)
-    private Vec3 redirectGetCenterOf(Vec3i pos) {
-        Ship ship = getShip();
-        Vec3 result = CreateCompat.getCenterOf(pos);
-        if (ship != null && this.source.getAirCurrentWorld() != null) {
-            Vector3d tempVec = new Vector3d();
-            ship.getTransform().getShipToWorld().transformPosition(result.x, result.y, result.z, tempVec);
-            result = VectorConversionsMCKt.toMinecraft(tempVec);
-        }
-        return result;
+        original.call(instance, motion);
     }
 }
