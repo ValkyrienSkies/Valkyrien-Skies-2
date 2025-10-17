9b1d95c0333e160e5628e15e2d46b3eb8e63e314
Bunting_chj <chj990707@postech.ac.kr>
2025-10-17 17:55:42 +0900

    Projectiles stuck on the ship will be sent to shipyard, and thus stay on the ship on movement or tp. This is mainly done for arrows and tridents, which can be stuck on the block surface.

---
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/MixinProjectile.java
@@ -0,0 +1,66 @@
+package org.valkyrienskies.mod.mixin.feature.shipyard_entities;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.TraceableEntity;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.HitResult.Type;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.entity.handling.DefaultShipyardEntityHandler;
+import org.valkyrienskies.mod.common.entity.handling.WorldEntityHandler;
+import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
+
+@Mixin(Projectile.class)
+public abstract class MixinProjectile extends Entity implements TraceableEntity {
+
+    @Shadow
+    protected abstract boolean canHitEntity(Entity entity);
+
+    public MixinProjectile(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    /**
+     * If the projectile hit the ship, sends it to the shipyard.
+     * This makes arrows/tridents/fishing hooks stuck on a ship render at correct position.
+     */
+    @Inject(
+        method = "onHit",
+        at = @At("HEAD")
+    )
+    private void sendToShipyard(HitResult hitResult, CallbackInfo ci) {
+        Ship ship;
+        if (hitResult instanceof BlockHitResult blockHitResult && (ship = VSGameUtilsKt.getShipManagingPos(level(), blockHitResult.getBlockPos())) != null) {
+            Vector3d hitLocation = ship.getWorldToShip().transformPosition(VectorConversionsMCKt.toJOML(blockHitResult.location));
+            blockHitResult.location = VectorConversionsMCKt.toMinecraft(hitLocation);
+            DefaultShipyardEntityHandler.INSTANCE.moveEntityFromWorldToShipyard(this, ship);
+        }
+    }
+
+    /**
+     * If the projectile is no longer touching any block on shipyard, return it to the world.
+     */
+    @Inject(
+        method = "tick",
+        at = @At("HEAD")
+    )
+    private void returnFromShipyard(CallbackInfo ci) {
+        final Ship ship;
+        if((ship = VSGameUtilsKt.getShipManaging(this)) == null) return;
+        HitResult result = ProjectileUtil.getHitResultOnMoveVector(this, this::canHitEntity);
+        if(result.getType() == Type.MISS) {
+            WorldEntityHandler.INSTANCE.moveEntityFromShipyardToWorld(this, ship);
+        }
+    }
+}
--- common/src/main/kotlin/org/valkyrienskies/mod/common/entity/handling/VSEntityManager.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/common/entity/handling/VSEntityManager.kt
@@ -5,6 +5,7 @@ import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.projectile.Projectile
 import org.valkyrienskies.mod.common.ValkyrienSkiesMod
 import org.valkyrienskies.mod.common.networking.PacketSyncVSEntityTypes
 import org.valkyrienskies.mod.common.util.MinecraftPlayer
@@ -74,7 +75,7 @@ object VSEntityManager {
             val className = entity::class.java.simpleName
             val registryName = BuiltInRegistries.ENTITY_TYPE.getKey(entity.type)
 
-            if (className.contains("SeatEntity", true) || registryName.path.contains(seatRegistryName)) {
+            if (entity is Projectile || className.contains("SeatEntity", true) || registryName.path.contains(seatRegistryName)) {
                 return DefaultShipyardEntityHandler
             }
         } catch (ex: Exception) {
