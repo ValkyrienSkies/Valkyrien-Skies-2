96500ed4db0f510e080cc2689138edd1bb5bb2dd
Bunting_chj <chj990707@postech.ac.kr>
2025-10-19 03:43:37 +0900

    Instead of adding ship chunks to the last of rebuild task, adds them to the first of tasks, making it the first to be rebuilt. RenderSection distance seems to do nothing at the moment but left them for insurance.

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/sodium/MixinRenderSection.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/sodium/MixinRenderSection.java
@@ -3,13 +3,16 @@ package org.valkyrienskies.mod.mixin.mod_compat.sodium;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import org.joml.Vector3d;
+import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.valkyrienskies.core.api.ships.ClientShip;
-import org.valkyrienskies.mod.common.VSClientGameUtils;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
-@Mixin(RenderSection.class)
+@Mixin(value = RenderSection.class, remap = false)
 public abstract class MixinRenderSection {
 
     @Shadow
@@ -23,16 +26,19 @@ public abstract class MixinRenderSection {
 
     /**
      * This mixin has the same purpose with {@link org.valkyrienskies.mod.mixin.feature.fix_render_chunk_sorting.MixinRenderChunk}
-     * Without this, ship chunks are considered far away from current position and always built last.
+     * Without this, ship chunks are considered far away from current position.
      */
     @WrapMethod(
         method = "getSquaredDistance(FFF)F"
     )
-    private float wrapIfInShipyard(float x, float y, float z, Operation<Float> original) {
-        final ClientShip ship = VSClientGameUtils.getClientShip(this.getCenterX(), this.getCenterY(), this.getCenterZ());
-        if (ship != null) {
-            final Vector3d cameraInShipyard = ship.getWorldToShip().transformPosition(x, y, z, new Vector3d());
-            return original.call((float)cameraInShipyard.x, (float)cameraInShipyard.y, (float)cameraInShipyard.z);
+    private float squareDistanceIfInShipyard(float x, float y, float z, Operation<Float> original) {
+        final ClientLevel world = Minecraft.getInstance().level;
+        final ClientShip shipObject = VSGameUtilsKt.getShipObjectManagingPos(world, this.getCenterX(), this.getCenterY(), this.getCenterZ());
+        if (shipObject != null) {
+            final Vector3dc chunkPosInWorld = shipObject.getRenderTransform().getShipToWorld().transformPosition(
+                new Vector3d(this.getCenterX(), this.getCenterY(), this.getCenterZ())
+            );
+            return (float) chunkPosInWorld.distanceSquared(x, y, z);
         }
         return original.call(x, y, z);
     }
