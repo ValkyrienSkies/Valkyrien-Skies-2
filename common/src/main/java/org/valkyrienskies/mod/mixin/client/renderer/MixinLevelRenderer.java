c54898279804338a919aefd843939417335bf1aa
Bunting_chj <chj990707@postech.ac.kr>
2025-10-18 12:19:58 +0900

    Rendering of the arrows stuck on ships is fixed. - If it is stuck on a wall and stays in the chunkClaim that holds no blocks, the chunk is not built and arrows are not rendered too. to fix that, entities in shipyard that is right next to the chunk border will bypass the chunk compile test. - On client side, arrow will be sent back from shipyard to world briefly for pickup animation.

--- common/src/main/java/org/valkyrienskies/mod/mixin/client/renderer/MixinLevelRenderer.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/client/renderer/MixinLevelRenderer.java
@@ -1,12 +1,23 @@
 package org.valkyrienskies.mod.mixin.client.renderer;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.core.BlockPos;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 @Mixin(LevelRenderer.class)
 public abstract class MixinLevelRenderer {
+
+    @Shadow
+    private @Nullable ClientLevel level;
 
     /**
      * @reason This mixin forces the game to always render block damage.
@@ -66,4 +77,24 @@ public abstract class MixinLevelRenderer {
 
      */
 
+    /**
+     * If an entity, for example an arrow stuck on a ship, is attached outside the border of the ship's chunk claim,
+     * it won't be rendered because the chunk isn't compiled.
+     * This injector bypasses that if the entity is in shipyard next to the compiled chunk.
+     */
+    @WrapOperation(
+        method = "renderLevel",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;isChunkCompiled(Lnet/minecraft/core/BlockPos;)Z")
+    )
+    private boolean isInShipyardBorder(LevelRenderer instance, BlockPos blockPos, Operation<Boolean> original){
+        if(original.call(instance, blockPos)) return true;
+        if(VSGameUtilsKt.isBlockInShipyard(level, blockPos)) {
+            BlockPos blockPos1 = blockPos.offset(-1, -1, -1);
+            BlockPos blockPos2 = blockPos.offset(1, 1, 1);
+            for(BlockPos neighbor : BlockPos.betweenClosed(blockPos1, blockPos2)) {
+                if (original.call(instance, neighbor)) return true;
+            }
+        }
+        return false;
+    }
 }
