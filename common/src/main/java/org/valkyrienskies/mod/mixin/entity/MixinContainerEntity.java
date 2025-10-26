daf57af2617f9fca072a381e4df9821ce9889e53
Bunting_chj <chj990707@postech.ac.kr>
2025-10-26 10:08:39 +0900

    Updated Create dependency. Fixed incompat with reach entity attributes in ContainerEntity. Fixed target of fabric super glue selection handler.

--- common/src/main/java/org/valkyrienskies/mod/mixin/entity/MixinContainerEntity.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/entity/MixinContainerEntity.java
@@ -1,13 +1,13 @@
 package org.valkyrienskies.mod.mixin.entity;
 
-import com.mojang.logging.LogUtils;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.world.Container;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.ContainerEntity;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
@@ -23,9 +23,11 @@ public interface MixinContainerEntity extends Container, MenuProvider {
      * @author Bunting_chj
      * @reason This is to restore Entities with storage spaces interactibility.
      */
-    @Overwrite
-    default boolean isChestVehicleStillValid(Player player) {
-        if(this.isRemoved()) return false;
+    @WrapMethod(
+        method = "isChestVehicleStillValid"
+    )
+    default boolean isChestVehicleStillValid(Player player, Operation<Boolean> original) {
+        if(original.call(player)) return true;
         return VSGameUtilsKt.squaredDistanceToInclShips(player, this.position().x, this.position().y, this.position().z) <= 8.0F * 8.0F;
     }
 }
--- fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/create/client/MixinSuperGlueSelectionHandler.java
+++ fabric/src/main/java/org/valkyrienskies/mod/fabric/mixin/compat/create/client/MixinSuperGlueSelectionHandler.java
@@ -5,6 +5,7 @@ import com.simibubi.create.content.contraptions.glue.SuperGlueSelectionHandler;
 import com.simibubi.create.foundation.utility.RaycastHelper;
 import java.util.Iterator;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -25,11 +26,11 @@ public abstract class MixinSuperGlueSelectionHandler {
     @Unique
     private Vec3 newTarget;
 
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/foundation/utility/RaycastHelper;getTraceOrigin(Lnet/minecraft/world/entity/player/Player;)Lnet/minecraft/world/phys/Vec3;"))
-    private Vec3 redirectGetTraceOrigin(Player playerIn) {
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;getEyePosition()Lnet/minecraft/world/phys/Vec3;"))
+    private Vec3 redirectGetTraceOrigin(final LocalPlayer playerIn) {
         Minecraft mc = Minecraft.getInstance();
         double range = ReachEntityAttributes.getReachDistance(playerIn, playerIn.isCreative() ? 5.0 : 4.5); // PlatformUtils.getReachDistance(playerIn) + 1;
-        Vec3 origin = RaycastHelper.getTraceOrigin(playerIn);
+        Vec3 origin = playerIn.getEyePosition();
         Vec3 target = RaycastHelper.getTraceTarget(playerIn, range, origin);
 
 
