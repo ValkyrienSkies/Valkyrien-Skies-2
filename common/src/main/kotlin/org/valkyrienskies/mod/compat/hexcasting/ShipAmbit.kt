051c8baec1731babead90bbc06e88c39d94ed11f
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-12-11 04:12:55 -0600

    Added `ShipAmbit#onHasEditPermissionsAt`

--- common/src/main/kotlin/org/valkyrienskies/mod/compat/hexcasting/ShipAmbit.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/compat/hexcasting/ShipAmbit.kt
@@ -1,9 +1,12 @@
 package org.valkyrienskies.mod.compat.hexcasting
 
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironmentComponent.HasEditPermissionsAt
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironmentComponent.IsVecInRange
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironmentComponent.Key
 import at.petrak.hexcasting.api.casting.eval.env.CircleCastEnv
+import net.beholderface.ephemera.api.toVec3i
+import net.minecraft.core.BlockPos
 import net.minecraft.world.phys.Vec3
 import org.valkyrienskies.core.api.util.GameTickOnly
 import org.valkyrienskies.mod.api.positionToShip
@@ -12,11 +15,21 @@ import org.valkyrienskies.mod.common.getLoadedShipManagingPos
 import org.valkyrienskies.mod.common.util.toJOML
 import java.util.UUID
 
-open class AmbitRemapping(val env: CastingEnvironment) : IsVecInRange {
+open class AmbitRemapping(val env: CastingEnvironment) : IsVecInRange, HasEditPermissionsAt {
     private val id = UUID.randomUUID()
     private val key = Key(id)
 
     override fun getKey(): Key<*> = key
+
+    @OptIn(GameTickOnly::class)
+    override fun onHasEditPermissionsAt(pos: BlockPos, current: Boolean): Boolean {
+        if (current) return true
+        // Always use Worldspace for permissions
+        env.world.getLoadedShipManagingPos(pos)?.let { ship ->
+            return env.hasEditPermissionsAt(BlockPos.containing(ship.positionToWorld(pos.center)))
+        }
+        return current
+    }
 
     @OptIn(GameTickOnly::class)
     override fun onIsVecInRange(vec: Vec3, current: Boolean): Boolean {
