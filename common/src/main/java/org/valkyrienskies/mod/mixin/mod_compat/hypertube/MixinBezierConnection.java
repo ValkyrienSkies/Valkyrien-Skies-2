5037f8a287c4d561d397b4bf8631a5326186dd0b
PedroRok <pedrolucasnmm@gmail.com>
2026-02-09 15:48:18 -0300

    [Cleanup] Remove unnecessary mixin and update mixins configuration

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/hypertube/MixinBezierConnection.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/hypertube/MixinBezierConnection.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
 
+// This is no longer necessary, but I’ll leave it here!
 @Pseudo
 @Mixin(targets = "com/pedrorok/hypertube/core/connection/BezierConnection", remap = false)
 public abstract class MixinBezierConnection {
