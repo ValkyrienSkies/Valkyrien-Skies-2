61fcc5c210e9a611ff29c79cdb8342468a8e23c7
Daniel99j <121533735+Daniel99j2@users.noreply.github.com>
2026-04-14 09:33:12 +0800

    Update MixinLevelRendererVanilla.java
    Fixes #1763

--- common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/vanilla_renderer/MixinLevelRendererVanilla.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/mod_compat/vanilla_renderer/MixinLevelRendererVanilla.java
@@ -231,13 +231,14 @@ public abstract class MixinLevelRendererVanilla implements LevelRendererDuck, Le
         }
     }
 
-    @WrapOperation(
-        method = "*",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;distSqr(Lnet/minecraft/core/Vec3i;)D")
-    )
-    private double distToShips(BlockPos from, Vec3i to, Operation<Double> distSqr){
-        return VSGameUtilsKt.squaredDistanceBetweenInclShips(level, from.getCenter(), Vec3.atCenterOf(to), distSqr);
-    }
+    // If ships stop rendering try uncommenting this line.
+//    @WrapOperation(
+//        method = "*",
+//        at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;distSqr(Lnet/minecraft/core/Vec3i;)D")
+//    )
+//    private double distToShips(BlockPos from, Vec3i to, Operation<Double> distSqr){
+//        return VSGameUtilsKt.squaredDistanceBetweenInclShips(level, from.getCenter(), Vec3.atCenterOf(to), distSqr);
+//    }
 
     @Inject(
         method = "*",
