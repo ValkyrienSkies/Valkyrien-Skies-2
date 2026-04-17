80520298fe767d8657a33f7c87ee0896b702c410
zyxkad <zyxkad@gmail.com>
2026-04-17 15:01:32 -0600

    prevent log spam that happens when player paused the game before the first tick

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/spawn_player_on_ship/MixinServerGamePacketListenerImpl.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/spawn_player_on_ship/MixinServerGamePacketListenerImpl.java
@@ -3,6 +3,7 @@ package org.valkyrienskies.mod.mixin.feature.spawn_player_on_ship;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.client.Minecraft;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -36,6 +37,11 @@ public abstract class MixinServerGamePacketListenerImpl {
     private void injectHandleMovePlayer(final ServerboundMovePlayerPacket packet, final CallbackInfo ci) {
         if (EntityShipCollisionUtils.isCollidingWithUnloadedShips(this.player)) {
             ci.cancel();
+            if (this.player.getServer().isSingleplayer() && Minecraft.getInstance().isPaused()) {
+                // Prevent log spam when player paused the world while ship loading
+                // (e.g. player is on a ship when they loading a world, and they unfocused the game that the world will be paused from the first tick)
+                return;
+            }
             LOGGER.warn("{} moved while colliding with unloaded ships!", this.player.getName().getString());
             this.teleport(this.player.getX(), this.player.getY(), this.player.getZ(), this.player.getYRot(),
                 this.player.getXRot());
