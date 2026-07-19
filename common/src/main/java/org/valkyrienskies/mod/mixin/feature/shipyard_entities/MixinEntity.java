2541e020e6c650bd5fe58a97f8acda4859655ea3
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2026-07-20 01:19:35 +1000

    allows shipyard entities to setpos to the shipyard

--- common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/MixinEntity.java
+++ common/src/main/java/org/valkyrienskies/mod/mixin/feature/shipyard_entities/MixinEntity.java
@@ -99,7 +99,7 @@ public abstract class MixinEntity {
         final BlockPos pos = BlockPos.containing(x, y, z);
         if (Level.isInSpawnableBounds(pos)) {
             lastSafePosition = new Vec3(x, y, z);
-        } else {
+        } else if (!VSEntityManager.INSTANCE.isShipyardEntity(Entity.class.cast(this))) {
             valkyrienskies$resolveInvalidPosition(x, y, z);
             ci.cancel();
             return;
